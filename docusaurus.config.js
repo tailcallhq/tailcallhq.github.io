d07d8afaa6a7eafebf040bbf017fea5c987615fa
Tushar Mathur <tusharmath@gmail.com>
2022-10-16 11:54:35 +0530

    chore: update deployment config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,7 +18,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: "tailcallhq", // Usually your GitHub org/user name.
-  projectName: "zio-compose", // Usually your repo name.
+  projectName: "tailcallhq.github.io", // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
