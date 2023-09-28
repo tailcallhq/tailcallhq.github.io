35ec783f594c3ff1b1a18ddcb123e4c57b562fbf
amit <amitksingh1490@gmail.com>
2023-09-28 15:59:21 +0530

    doc: update sidebar collapsible

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -41,6 +41,7 @@ const config = {
         },
         docs: {
           sidebarPath: require.resolve("./sidebars.js"),
+          sidebarCollapsible: false,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl: `https://github.com/${organization}/${project}/tree/develop`,
