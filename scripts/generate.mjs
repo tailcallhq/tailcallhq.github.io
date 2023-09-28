75974f9be77f8aef1942cb5b91a11de74630a47c
Rishi Raj Jain <rishi18304@iiitd.ac.in>
2023-09-28 17:32:31 +0530

    feat: Setup CD on develop branch (#11)
    * Create deploy.yml
    
    * Fix generate script
    
    * deploy
    
    * spacing
    
    * throw
    
    * add manual

--- scripts/generate.mjs
+++ scripts/generate.mjs
@@ -1,17 +1,26 @@
-import axios from "axios"
-import fs from "fs/promises"
+import fs from "fs";
+import path from "path";
+import axios from "axios";
 
 const generate = async () => {
-  const file = "./src/gen/tailcall-version.json"
-
-  console.log(`Generating ${file} ...`)
+  const file = "./src/gen/tailcall-version.json";
+  const directoryPath = path.dirname(file);
+  // Create the directory if it doesn't exist
+  if (!fs.existsSync(directoryPath)) {
+    fs.mkdirSync(directoryPath, { recursive: true });
+  }
+  console.log(`Generating ${file} ...`);
   const response = await axios.get(
     "https://api.github.com/repos/tailcallhq/tailcall/releases/latest"
-  )
+  );
+  if (response) {
+    fs.writeFileSync(file, JSON.stringify(response.data), "utf8");
+    console.log(`Generated ${file}`);
+  } else {
+    console.log(
+      "Failed to get a valid response from https://api.github.com/repos/tailcallhq/tailcall/releases/latest"
+    );
+  }
+};
 
-  fs.writeFile(file, JSON.stringify(response.data))
-
-  console.log(`Generated ${file}`)
-}
-
-generate()
+generate();
