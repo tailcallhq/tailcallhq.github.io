0f77141bf4076d72adf97e5f6828e79ede9c0677
Tushar Mathur <tusharmath@gmail.com>
2023-10-17 13:10:12 +0530

    update buttons styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -22,28 +22,26 @@ function HomepageHeader() {
             Get Started
           </Link>
           <Link className="button button--secondary button--lg" to="https://github.com/tailcallhq/tailcall/">
-            <div style={{alignItems: "center", display: "flex"}}>
-              <svg
-                stroke="currentColor"
-                fill="none"
-                stroke-width="3"
-                viewBox="0 0 24 24"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                height="24"
-                width="24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"></polygon>
-              </svg>
-              <span>&nbsp;Star</span>
-              <span
-                style={{transition: "max-width 1s", opacity: "1s"}}
-                className="w-full overflow-hidden whitespace-nowrap max-w-[100px] opacity-100"
-              >
-                &nbsp; {github.stargazers_count}
-              </span>
-            </div>
+            <svg
+              stroke="currentColor"
+              fill="none"
+              stroke-width="3"
+              viewBox="0 0 24 24"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              height="20"
+              width="20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"></polygon>
+            </svg>
+            <span>&nbsp;Star</span>
+            <span
+              style={{transition: "max-width 1s", opacity: "1s"}}
+              className="w-full overflow-hidden whitespace-nowrap max-w-[100px] opacity-100"
+            >
+              &nbsp; {github.stargazers_count}
+            </span>
           </Link>
         </div>
       </div>
