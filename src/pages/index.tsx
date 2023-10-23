eaf93c5c9572b8beb99d44963a5b71b98f04993d
Tushar Mathur <tusharmath@gmail.com>
2023-10-23 19:43:46 +0530

    style cleanups

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -33,14 +33,17 @@ function HomepageHeader() {
           <Link className="button button--primary button--lg" to="/docs/intro/quickstart">
             Get Started
           </Link>
-          <Link className="button button--secondary button--lg" to="https://github.com/tailcallhq/tailcall/">
+          <Link
+            className="button button--secondary button--lg star-button"
+            to="https://github.com/tailcallhq/tailcall/"
+          >
             <svg
               stroke="currentColor"
               fill="none"
-              stroke-width="3"
+              strokeWidth="3"
               viewBox="0 0 24 24"
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeLinecap="round"
+              strokeLinejoin="round"
               height="20"
               width="20"
               xmlns="http://www.w3.org/2000/svg"
