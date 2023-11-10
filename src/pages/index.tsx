acad6e0988afbdea39f8654ed6f71615375ae46e
Tushar Mathur <tusharmath@gmail.com>
2023-11-10 12:01:25 +0530

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -50,7 +50,7 @@ function HomepageHeader() {
             >
               <polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"></polygon>
             </svg>
-            <span>&nbsp;Star</span>
+            <span>&nbsp;Github</span>
             <span
               style={{transition: "max-width 1s", opacity: "1s"}}
               className="w-full overflow-hidden whitespace-nowrap max-w-[100px] opacity-100"
