022a7a2498987373f127c33190ce50a0b132bfe2
Tushar Mathur <tusharmath@gmail.com>
2023-11-27 17:19:28 +0530

    update home page

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -12,9 +12,10 @@ const FeatureList: FeatureItem[] = [
     title: "Orchestration",
     description: (
       <>
-        Harness the power of Tailcall's advanced orchestration<span className="highlight">primitives</span>, designed
-        for large-scale API management. Our platform supports complex compositions and data transformations, ensuring
-        seamless integration and reliability for enterprise operations.
+        Harness the power of Tailcall's advanced orchestration primitives, designed for large-scale API management.
+        Effortlessly interface with <span className="highlight">gRPC</span>,<span className="highlight">REST</span>,{" "}
+        <span className="highlight">GraphQL</span>, and more, ensuring seamless communication and complex data
+        transformations for enterprise operations.
       </>
     ),
   },
@@ -22,29 +23,21 @@ const FeatureList: FeatureItem[] = [
     title: "Governance",
     description: (
       <>
-        Implement robust governance across your API landscape using our{" "}
-        <span className="highlight">code-based governance</span> approach. Ensure compliance, standardization, and
-        operational consistency, vital for large-scale organizational needs.
+        Implement robust governance and security across your API landscape using our{" "}
+        <span className="highlight">code-based governance</span> and comprehensive security framework. Enjoy precise
+        control mechanisms that ensure compliance, standardization, operational consistency, and safeguard against
+        evolving digital threats.
       </>
     ),
   },
   {
-    title: "Performance",
+    title: "Efficiency",
     description: (
       <>
-        Tailcall brings breakthrough performance optimizations, critical for enterprises demanding{" "}
-        <span className="highlight">high-speed</span> and <span className="highlight">low-latency</span>. Expect
-        unmatched response times and significantly lower resource utilization.
-      </>
-    ),
-  },
-  {
-    title: "Security",
-    description: (
-      <>
-        Tailcall's comprehensive security framework is built to safeguard enterprise-grade API ecosystems. Enjoy peace
-        of mind with very <span className="highlight">precise control</span> mechanisms that secure your data and
-        operations against evolving digital threats.
+        Tailcall brings breakthrough performance optimizations, integrating{" "}
+        <span className="highlight">enterprise-specific tuning</span> for high-speed and low-latency. Expect unmatched
+        response times, lower resource utilization, and adaptability to your unique operational needs and workload
+        demands.
       </>
     ),
   },
@@ -59,39 +52,6 @@ const FeatureList: FeatureItem[] = [
     ),
   },
   {
-    title: "Runtime Tuning",
-    description: (
-      <>
-        Experience unparalleled adaptability with Tailcall's{" "}
-        <span className="highlight">enterprise-specific tuning</span>. Our platform intelligently adjusts to your unique
-        enterprise environment, optimizing API performance to align perfectly with your specific operational needs and
-        workload demands.
-      </>
-    ),
-  },
-  {
-    title: "Efficiency",
-    description: (
-      <>
-        Tailcall elevates IO efficiency through advanced <span className="highlight">caching and batching</span>{" "}
-        capabilities, enabling enterprises to streamline data processing. This feature reduces latency and enhances
-        throughput, making large-scale data handling more efficient and responsive.
-      </>
-    ),
-  },
-  {
-    title: "Protocol Agnostic",
-    description: (
-      <>
-        Embrace unparalleled connectivity with Tailcall's protocol-agnostic architecture. Effortlessly interface with{" "}
-        <span className="highlight">gRPC</span>, <span className="highlight">REST</span>,{" "}
-        <span className="highlight">GraphQL</span>, and more, ensuring seamless communication across a diverse range of
-        API protocols. This flexibility is key for enterprises looking to integrate a broad spectrum of services and
-        data sources.
-      </>
-    ),
-  },
-  {
     title: "Extendable",
     description: (
       <>
@@ -101,27 +61,14 @@ const FeatureList: FeatureItem[] = [
       </>
     ),
   },
-  {
-    title: "Cloud Native",
-    description: (
-      <>
-        Optimized for cloud environments, Tailcall leverages the scalability and flexibility of{" "}
-        <span className="highlight">cloud infrastructure</span>, making it an ideal choice for enterprises seeking
-        efficient deployment and management of their API operations in the cloud.
-      </>
-    ),
-  },
 ]
 
-function Feature({idx, title, description}: FeatureItem) {
+function Feature({idx, data: {title, description}}: {data: FeatureItem; idx: number}) {
   return (
-    <div className={clsx("col col--6")}>
+    <div className={clsx("col col--4")}>
       <div className="text--center">{/* <Svg className={styles.featureSvg} role="img" /> */}</div>
       <div className="text--left padding-horiz--md">
-        <h3>
-          {" "}
-          {idx + 1}. {title}
-        </h3>
+        <h3>{title}</h3>
         <p>{description}</p>
       </div>
     </div>
@@ -129,18 +76,18 @@ function Feature({idx, title, description}: FeatureItem) {
 }
 
 export default function HomepageFeatures(): JSX.Element {
-  const INITIAL = 2
+  const INITIAL = 3
   return (
     <section className={styles.features}>
       <div className="container">
         <div className="row">
           {FeatureList.slice(0, INITIAL).map((props, idx) => (
-            <Feature key={idx} {...{...props, idx}} />
+            <Feature key={idx} data={props} idx={idx} />
           ))}
         </div>
         <div className="row row-center">
           {FeatureList.slice(INITIAL).map((props, idx) => (
-            <Feature key={idx} {...{...props, idx: idx + INITIAL}} />
+            <Feature key={idx} data={props} idx={idx + INITIAL} />
           ))}
         </div>
       </div>
