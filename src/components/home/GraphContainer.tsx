c12ece7e914ed209e4ac0d3830e99bae608db6b9
vvu060 <vvu060@gmail.com>
2023-12-07 10:23:19 +0530

    Add GraphContainer component

---
+++ src/components/home/GraphContainer.tsx
@@ -0,0 +1,29 @@
+import React from "react"
+import CountUp from "react-countup"
+
+type GraphContainerProps = {
+  metricTitle: string
+  metricData: number
+  metricDesc: string
+}
+
+const GraphContainer = ({metricTitle, metricData, metricDesc}: GraphContainerProps) => {
+  return (
+    <div
+      style={{
+        border: "1px solid #2C2C2C",
+      }}
+      className="border border-red-500 h-[400px] w-[95%] sm:w-[680px] rounded-3xl sm:rounded-[32px]"
+    >
+      <div className="flex flex-col px-12 py-8">
+        <span className="text-content-small sm:text-content-medium text-tailCall-light-100">{metricTitle}</span>
+        <span className="text-title-medium sm:text-title-large text-tailCall-light-100">
+          <CountUp start={2000} end={+metricData} decimals={2} duration={0.75} />
+        </span>
+        <span className="text-content-tiny sm:text-content-small text-tailCall-light-400">{metricDesc}</span>
+      </div>
+    </div>
+  )
+}
+
+export default GraphContainer
