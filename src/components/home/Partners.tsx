7d017a84345bc094f9705cb34437397a57db3e59
vvu060 <vvu060@gmail.com>
2023-12-07 10:37:01 +0530

    Update partner image component names

--- src/components/home/Partners.tsx
+++ src/components/home/Partners.tsx
@@ -15,7 +15,7 @@ const Partners = () => {
         <div className="hidden sm:flex space-x-16 mt-10 overflow-hidden">
           {partnerImages.map((partner) => (
             <div key={partner.id} className="h-20">
-              <partner.Svg role="img" />
+              <partner.logo role="img" />
             </div>
           ))}
         </div>
@@ -23,7 +23,7 @@ const Partners = () => {
       <div className="sm:hidden flex items-center justify-around flex-wrap mt-6 space-y-2">
         {partnerImages.map((partner) => (
           <div key={partner.id} className="h-20">
-            <partner.Svg role="img" />
+            <partner.logo role="img" />
           </div>
         ))}
       </div>
