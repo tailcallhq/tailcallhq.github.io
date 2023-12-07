02457ecd944e5177fad66fcf16afdf7f0ed2a649
vvu060 <vvu060@gmail.com>
2023-12-07 10:02:49 +0530

    add assets, constants and types for project

---
+++ src/types/index.d.ts
@@ -0,0 +1,27 @@
+type PartnerImage = {
+  id: number
+  name: string
+  Svg: React.FunctionComponent<React.SVGProps<SVGSVGElement>>
+}
+
+type Feature = {
+  id: number
+  Svg: React.FunctionComponent<React.SVGProps<SVGSVGElement>>
+  title: string
+  description1: string
+  highlightedText: string
+  description2: string
+}
+
+type MoreFeatures = {
+  id: number
+  title: string
+  logo: React.FunctionComponent<React.SVGProps<SVGSVGElement>>
+}
+
+type Social = {
+  id: number
+  name: string
+  image: React.FunctionComponent<React.SVGProps<SVGSVGElement>>
+  href: string
+}
