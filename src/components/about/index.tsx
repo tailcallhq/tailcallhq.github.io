4a65561b9d4c2a4e5e546a9c578eab8e7ed1c19c
vvu060 <vvu060@gmail.com>
2023-12-08 11:07:39 +0530

    Add AboutPage component

---
+++ src/components/about/index.tsx
@@ -0,0 +1,16 @@
+import React from "react"
+import Hero from "./Hero"
+import WhoWeAre from "./WhoWeAre"
+import Founders from "./Founders"
+
+const AboutPage = () => {
+  return (
+    <>
+      <Hero />
+      <WhoWeAre />
+      <Founders />
+    </>
+  )
+}
+
+export default AboutPage
