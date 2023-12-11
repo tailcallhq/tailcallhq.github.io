e2043e8d646aaf80b1699bc7b4ec53b8dd0d2f7d
vvu060 <vvu060@gmail.com>
2023-12-11 09:08:10 +0530

    Add Partners component to EnterprisePage

--- src/components/enterprise/index.tsx
+++ src/components/enterprise/index.tsx
@@ -1,10 +1,12 @@
 import React from "react"
 import Hero from "./Hero"
+import Partners from "../home/Partners"
 
 const EnterprisePage = () => {
   return (
     <div>
       <Hero />
+      <Partners />
     </div>
   )
 }
