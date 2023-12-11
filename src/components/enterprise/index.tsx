2f0113f20a9d6ad7888d701a8725e3daf263058d
vvu060 <vvu060@gmail.com>
2023-12-11 09:43:23 +0530

    Add EnterpriseFeatures component to EnterprisePage

--- src/components/enterprise/index.tsx
+++ src/components/enterprise/index.tsx
@@ -1,12 +1,14 @@
 import React from "react"
 import Hero from "./Hero"
 import Partners from "../home/Partners"
+import EnterpriseFeatures from "./EnterpriseFeatures"
 
 const EnterprisePage = () => {
   return (
     <div>
       <Hero />
       <Partners />
+      <EnterpriseFeatures />
     </div>
   )
 }
