e79b891ba0e5c002f2becfde688b76e83ce09063
vvu060 <vvu060@gmail.com>
2023-12-11 08:21:10 +0530

    make hero component responsive

--- src/components/about/Hero.tsx
+++ src/components/about/Hero.tsx
@@ -4,15 +4,15 @@ import AboutUs from "@site/static/images/about/about-us-visual.svg"
 
 const Hero = () => {
   return (
-    <section className="flex items-center justify-between bg-tailCall-dark-500 w-full h-full">
-      <div className="m-20">
+    <section className="flex flex-col sm:flex-row items-end sm:items-center justify-between bg-tailCall-dark-500 w-full h-full">
+      <div className="mx-4 my-6 sm:m-20">
         <SectionTitle title="Our Mission" />
-        <p className="text-display-small text-tailCall-light-100 max-w-3xl">
+        <p className="text-title-small sm:text-display-small text-tailCall-light-100 max-w-3xl">
           Simplify complex API interactions through a tailor-made GraphQL solution, ensuring efficient data retrieval
           and optimised query handling.
         </p>
       </div>
-      <AboutUs />
+      <AboutUs className="h-40 w-60 sm:h-fit sm:w-fit" />
     </section>
   )
 }
