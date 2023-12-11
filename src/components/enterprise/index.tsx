3d34f204bdcb7c02f97874a9f17524b0a979c424
vvu060 <vvu060@gmail.com>
2023-12-11 10:09:38 +0530

    Add MoreEnterpriseFeatures component

--- src/components/enterprise/index.tsx
+++ src/components/enterprise/index.tsx
@@ -2,6 +2,7 @@ import React from "react"
 import Hero from "./Hero"
 import Partners from "../home/Partners"
 import EnterpriseFeatures from "./EnterpriseFeatures"
+import MoreEnterpriseFeatures from "./MoreEnterpriseFeatures"
 
 const EnterprisePage = () => {
   return (
@@ -9,6 +10,7 @@ const EnterprisePage = () => {
       <Hero />
       <Partners />
       <EnterpriseFeatures />
+      <MoreEnterpriseFeatures />
     </div>
   )
 }
