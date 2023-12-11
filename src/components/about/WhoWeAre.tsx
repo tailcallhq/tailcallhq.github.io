f4d894f15119af3387acb0bc6f5178fd6e33bee0
vvu060 <vvu060@gmail.com>
2023-12-11 08:26:00 +0530

    make who we are comp responsive

--- src/components/about/WhoWeAre.tsx
+++ src/components/about/WhoWeAre.tsx
@@ -3,12 +3,12 @@ import SectionTitle from "../shared/SectionTitle"
 
 const WhoWeAre = () => {
   return (
-    <section className="mt-20 flex items-start space-x-20 px-40">
+    <section className="mt-8 sm:mt-20 flex flex-col sm:flex-row items-start sm:space-x-20 px-8 sm:px-40">
       <div className="min-w-fit">
         <SectionTitle title="About Us" />
-        <p className="text-display-small bg-tailCall-yellow rounded-lg mt-2">Who we are?</p>
+        <p className="text-title-semi-large sm:text-display-small bg-tailCall-yellow rounded-lg mt-2">Who we are?</p>
       </div>
-      <p className="text-content-medium">
+      <p className="text-content-tiny sm:text-content-medium">
         We are change agents in the world of API development, focusing our expertise on enhancing GraphQL technologies.
         Our team is composed of seasoned engineers, innovators, and strategists committed to reshaping the API
         landscape. We believe in breaking barriers, pushing boundaries, and creating solutions that empower developers
