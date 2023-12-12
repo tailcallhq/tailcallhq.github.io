68ea8e63a89b4e917d4544087f9d41554d36cd17
vvu060 <vvu060@gmail.com>
2023-12-12 11:49:29 +0530

    make more features section responsive

--- src/components/enterprise/MoreEnterpriseFeatures.tsx
+++ src/components/enterprise/MoreEnterpriseFeatures.tsx
@@ -5,9 +5,11 @@ import {additionalEnterpriseFeatures} from "@site/src/constants"
 const MoreEnterpriseFeatures = () => {
   return (
     <section>
-      <h4 className="text-display-tiny text-center mb-12">Additional enterprise features</h4>
+      <h4 className="text-title-large sm:text-display-tiny text-center mb-6 sm:mb-12">
+        Additional enterprise features
+      </h4>
 
-      <div className="flex items-center gap-8 justify-center flex-wrap">
+      <div className="flex items-center gap-4 sm:gap-8 justify-center flex-wrap">
         {additionalEnterpriseFeatures.map((feature) => (
           <div
             className="flex items-center px-4 py-3 rounded w-fit space-x-4"
@@ -15,7 +17,7 @@ const MoreEnterpriseFeatures = () => {
               border: "1px solid #E5E5E5",
             }}
           >
-            <feature.logo className="h-6 w-6" />
+            <feature.logo className="h-4 w-4 sm:h-6 sm:w-6" />
             <span className="text-title-tiny">{feature.title}</span>
           </div>
         ))}
