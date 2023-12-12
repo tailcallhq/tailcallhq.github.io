f0d110e26445333f41f1157bf9ab95638a54f3b3
vvu060 <vvu060@gmail.com>
2023-12-12 11:49:43 +0530

    make pricing section responsive

--- src/components/enterprise/Pricing.tsx
+++ src/components/enterprise/Pricing.tsx
@@ -6,21 +6,21 @@ import Button from "../shared/Button"
 
 const Pricing = () => {
   return (
-    <section className="my-24">
-      <h4 className="text-display-small text-center mb-10">
-        <span className="rounded-lg bg-tailCall-yellow px-1">Plans</span> that fit your scale.
+    <section className="my-14 sm:my-24">
+      <h4 className="text-title-large sm:text-display-small text-center mb-6 sm:mb-10">
+        <span className="rounded sm:rounded-lg bg-tailCall-yellow px-1">Plans</span> that fit your scale.
       </h4>
 
-      <div className="flex items-center justify-center gap-x-6">
+      <div className="flex flex-col sm:flex-row items-center justify-center gap-6">
         {pricingPlans.map((plan) => (
           <div
             key={plan.id}
             style={{
               border: plan.mostPopular ? "1px solid #000" : "1px solid #E7E7E7",
-              width: "384px",
-              height: "584px",
+              //   width: "384px",
+              //   height: "584px",
             }}
-            className="rounded-2xl flex flex-col justify-between py-6 relative"
+            className="rounded-2xl flex flex-col justify-between gap-y-7 sm:gap-y-0 py-4 sm:py-6 relative w-[328px] h-auto sm:w-[384px] sm:h-[584px]"
           >
             {plan.mostPopular && (
               <p className="absolute -top-3.5 left-[50%] transform -translate-x-[50%] bg-tailCall-dark-600 text-tailCall-light-100 p-1 rounded text-content-tiny font-bold">
@@ -29,14 +29,14 @@ const Pricing = () => {
             )}
 
             <div className="flex flex-col">
-              <span className="text-content-medium text-center">{plan.name}</span>
-              <span className="text-display-tiny text-center">{plan.price}</span>
-              <span className="text-content-small text-center">{plan.for}</span>
-              <span className="text-content-small text-center">{plan.billing}</span>
-              <span className="text-content-small text-center">{plan.volumeDiscounts}</span>
+              <span className="text-content-small sm:text-content-medium text-center">{plan.name}</span>
+              <span className="text-title-large sm:text-display-tiny text-center">{plan.price}</span>
+              <span className="text-content-small sm:text-content-small text-center">{plan.for}</span>
+              <span className="text-content-small sm:text-content-small text-center">{plan.billing}</span>
+              <span className="text-content-small sm:text-content-small text-center">{plan.volumeDiscounts}</span>
             </div>
 
-            <div className="flex flex-col items-start justify-start">
+            <div className="flex flex-col items-start justify-start gap-4 px-8">
               {plan.features.map((feature) => (
                 <div key={feature.id} className="flex items-center space-x-3 m-0 p-0">
                   <CheckCircle className="h-5 w-5" />
@@ -45,7 +45,9 @@ const Pricing = () => {
               ))}
             </div>
 
-            <Button title={plan.buttonText} onClick={() => {}} theme="light" />
+            <div className="text-center mx-4">
+              <Button title={plan.buttonText} onClick={() => {}} theme={plan.mostPopular ? "dark" : "light"} />
+            </div>
           </div>
         ))}
       </div>
