efedf5309021b1eca7209e50eca178b72a985ae8
vvu060 <vvu060@gmail.com>
2023-12-13 12:15:29 +0530

    fix git overflow issue

--- src/components/about/Investors.tsx
+++ src/components/about/Investors.tsx
@@ -7,7 +7,7 @@ import {investors} from "@site/src/constants"
 const Investors = () => {
   return (
     <section className="h-full w-full relative">
-      <Grid className="absolute inset-0 -z-10" />
+      <Grid className="absolute inset-0 -z-10 w-full" />
       <p className="text-title-semi-large sm:text-display-tiny text-center mx-10 sm:mx-0 sm:mt-9">
         We are backed by <span className="bg-tailCall-yellow rounded-lg px-2">incredible investors.</span>
       </p>
--- src/components/about/SocialBuzz.tsx
+++ src/components/about/SocialBuzz.tsx
@@ -7,7 +7,7 @@ import Tweet2 from "@site/static/images/about/tweet-2.jpg"
 const SocialBuzz = () => {
   return (
     <section className="my-16 sm:my-24 relative flex items-center mx-4 sm:mx-auto sm:space-x-10 max-w-8xl">
-      <Grid className="absolute inset-0 -z-10" />
+      <Grid className="absolute inset-0 -z-10 w-full" />
       <div className="flex flex-col gap-6 sm:gap-y-44">
         <div className="ml-4 sm:ml-20">
           <SectionTitle title="About Us" />
