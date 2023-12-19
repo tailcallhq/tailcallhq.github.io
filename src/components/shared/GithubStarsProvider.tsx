228198f5636d35297591955a2d00091f8ae6438c
Rajat <rajatbarman@yahoo.com>
2023-12-19 20:31:37 +0530

    Stop flickering of stars count

--- src/components/shared/GithubStarsProvider.tsx
+++ src/components/shared/GithubStarsProvider.tsx
@@ -2,22 +2,24 @@ import React, {createContext, useEffect, useState} from "react"
 
 export const GithubStarsContext = createContext(null)
 
-const sessionStorage = {
+const storage = {
   get(key) {
-    if (typeof window !== "undefined" && window.sessionStorage) {
-      return window.sessionStorage.getItem(key)
+    if (typeof window !== "undefined" && window["__tc_data__"]) {
+      return window["__tc_data__"][key]
     }
+    return null
   },
 
   set(key, val) {
-    if (typeof window !== "undefined" && window.sessionStorage) {
-      return window.sessionStorage.setItem(key, val)
+    if (typeof window !== "undefined") {
+      window["__tc_data__"] = window["__tc_data__"] || {}
+      window["__tc_data__"][key] = val
     }
   },
 }
 
 export default function GithubStarsProvider({children}) {
-  const [starsCount, setStarsCount] = useState("")
+  const [starsCount, setStarsCount] = useState(storage.get("githubStars"))
 
   function fetchGithubStars() {
     return fetch("https://api.github.com/repos/tailcallhq/tailcall")
@@ -27,16 +29,12 @@ export default function GithubStarsProvider({children}) {
       .then((resp) => {
         const respStarsCount = resp.stargazers_count || "250"
         setStarsCount(respStarsCount)
-        sessionStorage.set("githubStars", respStarsCount)
+        storage.set("githubStars", respStarsCount)
         return respStarsCount
       })
   }
 
   useEffect(() => {
-    let starsCountInSessionStorage = sessionStorage.get("githubStars")
-    if (starsCountInSessionStorage) {
-      return setStarsCount(starsCountInSessionStorage)
-    }
     fetchGithubStars()
   }, [])
 
