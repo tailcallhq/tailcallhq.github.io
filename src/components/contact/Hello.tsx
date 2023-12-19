e6225777cc9a4da980b33e234713e2219c4e7221
vvu060 <vvu060@gmail.com>
2023-12-19 13:24:33 +0530

    Update Hello component styling

--- src/components/contact/Hello.tsx
+++ src/components/contact/Hello.tsx
@@ -49,7 +49,7 @@ const Hello = () => {
     <section className="relative h-auto">
       <Grid className="absolute inset-0 -z-10 h-[540px] w-full" />
 
-      <div className="p-6 sm:py-10 lg:py-20 flex flex-col lg:flex-row items-center lg:items-start justify-center lg:space-x-40">
+      <div className="p-6 sm:py-10 lg:py-20 flex flex-col lg:flex-row items-center lg:items-start justify-center lg:space-x-40 xl:h-[72vh]">
         <h2 className="text-title-large text-center sm:text-left sm:text-display-medium lg:text-display-large lg:max-w-md">
           Say <span className="bg-tailCall-yellow rounded sm:rounded-2xl px-1 sm:px-2">hello</span> to us!
         </h2>
