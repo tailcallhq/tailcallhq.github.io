2d480d363154cd0160cbee4cf424cfaf1f4db2f1
vvu060 <vvu060@gmail.com>
2023-12-22 16:43:48 +0530

    add animation for button

--- src/components/shared/Button.tsx
+++ src/components/shared/Button.tsx
@@ -1,5 +1,5 @@
 import Link from "@docusaurus/Link"
-import React from "react"
+import React, {useEffect} from "react"
 import {SVGProps} from "react"
 
 type ButtonProps = {
@@ -13,30 +13,41 @@ type ButtonProps = {
 }
 
 const Button = ({title, Icon, theme, onClick, href, width, disabled}: ButtonProps) => {
+  const [isHovered, setIsHovered] = React.useState(false)
+
   const generateThemeClasses = () => {
     switch (theme) {
       case "light":
         return {
           classes: "text-tailCall-dark-500 bg-transparent",
-          styles: "2px solid #121315",
+          styles: isHovered ? "2px solid #121315" : "1px solid #121315",
+          gridClasses:
+            "absolute inset-0 -z-20 w-full h-full opacity-0 scale-90 group-hover:scale-100 group-hover:opacity-10 transform transition ease-out duration-150",
         }
 
       case "dark":
         return {
-          classes: "text-tailCall-light-100 bg-tailCall-dark-600 border-none",
-          styles: "none",
+          classes: "text-tailCall-light-100 bg-white border-none",
+          styles: "2px solid #121315",
+          gridClasses:
+            "absolute inset-0 z-10 w-full h-full opacity-0 scale-90 group-hover:scale-95 group-hover:opacity-100 transform transition ease-out duration-150",
         }
 
       case "gray":
         return {
           classes: "text-tailCall-dark-100 bg-transparent text-tailCall-light-100",
           styles: "2px solid #FFFFFF",
+          gridClasses: "hidden",
         }
 
       default:
         break
     }
   }
+
+  useEffect(() => {
+    generateThemeClasses()
+  }, [isHovered])
 
   return (
     <Link
@@ -48,7 +59,9 @@ const Button = ({title, Icon, theme, onClick, href, width, disabled}: ButtonProp
       <button
         disabled={disabled}
         onClick={onClick}
-        className={`disabled:opacity-25 disabled:cursor-not-allowed flex items-center justify-center space-x-3 no-underline rounded-lg sm:rounded-xl h-12 sm:h-16 text-content-small font-bold sm:text-title-small cursor-pointer px-6 py-3 sm:px-8 lg:px-10 sm:py-4 lg:py-5 
+        onMouseOver={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+        className={`group relative disabled:opacity-25 disabled:cursor-not-allowed flex items-center justify-center gap-x-3 no-underline rounded-lg sm:rounded-xl h-12 sm:h-16 text-content-small font-bold sm:text-title-small cursor-pointer px-6 py-3 sm:px-8 lg:px-10 sm:py-4 lg:py-5 
         ${generateThemeClasses().classes ?? ""}
         `}
         style={{
@@ -56,8 +69,22 @@ const Button = ({title, Icon, theme, onClick, href, width, disabled}: ButtonProp
           border: generateThemeClasses().styles,
         }}
       >
+        {theme === "dark" && (
+          <>
+            <div
+              className={`lg:block rounded-md lg:rounded-lg absolute inset-0 w-full group-hover:z-0 bg-tailCall-dark-500 lg:group-hover:scale-x-[0.98] lg:group-hover:scale-y-[0.95] transform transition ease-out duration-150`}
+            />
+            {!disabled && (
+              <div className="hidden lg:block button-grid-bg-section h-full w-full scale-90 opacity-0 group-hover:scale-[0.98] group-hover:opacity-100 transform transition ease-out duration-150" />
+            )}
+          </>
+        )}
+
+        {theme === "light" && !disabled && (
+          <div className="hidden lg:block button-grid-bg-section-dark h-full w-full scale-90 opacity-0 group-hover:scale-[1] group-hover:opacity-100 transform transition ease-out duration-150" />
+        )}
         {Icon && <Icon className="w-6 h-6 sm:w-7 sm:h-7 lg:h-8 lg:w-8 text-white" />}
-        {title && <span> {title}</span>}
+        {title && <span className="z-20"> {title}</span>}
       </button>
     </Link>
   )
