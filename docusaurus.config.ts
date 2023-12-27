c03a13276d022db2d988673d3568cde70a76723f
Rajat <rajatbarman@yahoo.com>
2023-12-27 17:43:34 +0530

    Sourcing docs from the main tailcall repo

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -6,60 +6,60 @@ const title = "Tailcall"
 const organization = "tailcallhq"
 const project = "tailcallhq.github.io"
 
-// function fetchRemoteContentConfig(author = "rajatbarman", repo = "tc-docs", branch = "main") {
-//   console.log(`Fetching docs content from ${author}/${repo}`)
-//   return fetch(`https://api.github.com/repos/${author}/${repo}/git/trees/${branch}?recursive=1`, {
-//     // headers: {
-//     //   Authorization: "Bearer <pat token>",
-//     // },
-//   })
-//     .then((resp) => {
-//       return resp.json()
-//     })
-//     .then((resp) => {
-//       if (resp.message) {
-//         console.log(`\x1b[31m${resp.message}\x1b[0m`)
-//       }
-//       const config = []
-//       const docs = resp.tree?.filter((doc) => {
-//         if (doc.path.startsWith("docs/")) {
-//           return true
-//         }
-//       })
-//       const folders = docs?.filter((doc) => {
-//         return doc.type === "tree"
-//       })
-//       folders.forEach((folder) => {
-//         const folderName = folder.path.replace("docs/", "")
-//         config.push([
-//           "docusaurus-plugin-remote-content",
-//           {
-//             name: `${folderName}_docs`, // used by CLI, must be path safe
-//             sourceBaseUrl: `https://raw.githubusercontent.com/${author}/${repo}/${branch}/docs/${folderName}`, // the base url for the markdown (gets prepended to all of the documents when fetching)
-//             outDir: `docs/${folderName}`, // the base directory to output to.
-//             documents: docs
-//               .filter((doc) => {
-//                 return (
-//                   doc.type === "blob" &&
-//                   doc.path.startsWith(`docs/${folderName}`) &&
-//                   (doc.path.endsWith(".md") || doc.path.endsWith(".mdx"))
-//                 )
-//               })
-//               .map((doc) => {
-//                 return encodeURIComponent(doc.path.replace(`docs/${folderName}/`, ""))
-//               }),
-//           },
-//         ])
-//       })
-//       return config
-//     })
-//     .catch((err) => {
-//       console.log(err)
-//     })
-// }
+function fetchRemoteContentConfig(author = "tailcallhq", repo = "tailcall", branch = "main") {
+  console.log(`Fetching docs content from ${author}/${repo}`)
+  return fetch(`https://api.github.com/repos/${author}/${repo}/git/trees/${branch}?recursive=1`, {
+    // headers: {
+    //   Authorization: "Bearer <pat token>",
+    // },
+  })
+    .then((resp) => {
+      return resp.json()
+    })
+    .then((resp) => {
+      if (resp.message) {
+        console.log(`\x1b[31m${resp.message}\x1b[0m`)
+      }
+      const config = []
+      const docs = resp.tree?.filter((doc) => {
+        if (doc.path.startsWith("docs/")) {
+          return true
+        }
+      })
+      const folders = docs?.filter((doc) => {
+        return doc.type === "tree"
+      })
+      folders.forEach((folder) => {
+        const folderName = folder.path.replace("docs/", "")
+        config.push([
+          "docusaurus-plugin-remote-content",
+          {
+            name: `${folderName}_docs`, // used by CLI, must be path safe
+            sourceBaseUrl: `https://raw.githubusercontent.com/${author}/${repo}/${branch}/docs/${folderName}`, // the base url for the markdown (gets prepended to all of the documents when fetching)
+            outDir: `docs/${folderName}`, // the base directory to output to.
+            documents: docs
+              .filter((doc) => {
+                return (
+                  doc.type === "blob" &&
+                  doc.path.startsWith(`docs/${folderName}`) &&
+                  (doc.path.endsWith(".md") || doc.path.endsWith(".mdx"))
+                )
+              })
+              .map((doc) => {
+                return encodeURIComponent(doc.path.replace(`docs/${folderName}/`, ""))
+              }),
+          },
+        ])
+      })
+      return config
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+}
 
 export default async function () {
-  // const remoteContentPluginConfig = await fetchRemoteContentConfig()
+  const remoteContentPluginConfig = await fetchRemoteContentConfig()
   return {
     title,
     trailingSlash: true,
@@ -167,7 +167,7 @@ export default async function () {
       tableOfContents: {},
     } satisfies Preset.ThemeConfig,
     plugins: [
-      // ...(remoteContentPluginConfig || []),
+      ...(remoteContentPluginConfig || []),
       [
         require.resolve("docusaurus-lunr-search"),
         {
