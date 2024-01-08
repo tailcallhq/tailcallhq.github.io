a6a067787df0c76cb29436023c09fd6a9458b700
vvu060 <vvu060@gmail.com>
2024-01-08 10:25:34 +0530

    Update Button component styles

--- src/components/shared/Button.tsx
+++ src/components/shared/Button.tsx
@@ -17,21 +17,21 @@ const Button = ({title, Icon, theme, onClick, href, width, disabled}: ButtonProp
     switch (theme) {
       case "light":
         return {
-          classes: "text-tailCall-dark-500 bg-transparent",
+          classes: "text-tailCall-dark-500 bg-transparent hover:text-tailCall-dark-500",
           styles: "1px solid var(--ifm-color-brand-dark-100)",
           gridClasses: "",
         }
 
       case "dark":
         return {
-          classes: "text-tailCall-light-100 bg-white border-none",
+          classes: "text-tailCall-light-100 bg-white border-none hover:text-tailCall-light-100",
           styles: "2px solid var(--ifm-color-brand-dark-100)",
           gridClasses: "",
         }
 
       case "gray":
         return {
-          classes: "text-tailCall-dark-100 bg-transparent text-tailCall-light-100",
+          classes: "text-tailCall-light-100 bg-transparent hover:text-tailCall-light-100",
           styles: "2px solid var(--ifm-color-white)",
           gridClasses: "hidden",
         }
@@ -48,38 +48,32 @@ const Button = ({title, Icon, theme, onClick, href, width, disabled}: ButtonProp
   return (
     <Link
       to={href || "#"}
+      onClick={onClick}
+      className={`group relative disabled:opacity-25 disabled:cursor-not-allowed flex items-center justify-center gap-x-3 no-underline rounded-lg sm:rounded-xl h-12 sm:h-16 text-content-small font-bold sm:text-title-small cursor-pointer px-6 py-3 sm:px-8 lg:px-10 sm:py-4 lg:py-5 
+      ${generateThemeClasses().classes ?? ""} ${disabled ? "cursor-not-allowed opacity-20" : ""}
+      `}
       style={{
-        textDecorationLine: "none",
+        width: width ? width : "fit-content",
+        border: generateThemeClasses().styles,
+        textDecoration: "none",
       }}
     >
-      <button
-        disabled={disabled}
-        onClick={onClick}
-        className={`group relative disabled:opacity-25 disabled:cursor-not-allowed flex items-center justify-center gap-x-3 no-underline rounded-lg sm:rounded-xl h-12 sm:h-16 text-content-small font-bold sm:text-title-small cursor-pointer px-6 py-3 sm:px-8 lg:px-10 sm:py-4 lg:py-5 
-        ${generateThemeClasses().classes ?? ""}
-        `}
-        style={{
-          width: width ? width : "fit-content",
-          border: generateThemeClasses().styles,
-        }}
-      >
-        {theme === "dark" && (
-          <>
-            <div
-              className={`lg:block rounded-md lg:rounded-lg absolute inset-0 w-full bg-tailCall-dark-500 group-hover:lg:scale-x-[0.98] group-hover:lg:scale-y-[0.95] transform transition-all ease-out duration-250`}
-            />
-            {!disabled && (
-              <div className="hidden lg:block button-grid-bg-section h-full w-full scale-90 opacity-0 group-hover:scale-[0.98] group-hover:opacity-100 transform transition-all ease-out duration-250" />
-            )}
-          </>
-        )}
+      {theme === "dark" && (
+        <>
+          <div
+            className={`lg:block rounded-md lg:rounded-lg absolute inset-0  w-full bg-tailCall-dark-500 group-hover:lg:scale-x-[0.98] group-hover:lg:scale-y-[0.95] transform transition-all ease-out duration-250`}
+          />
+          {!disabled && (
+            <div className="hidden lg:block button-grid-bg-section h-full w-full scale-90 opacity-0 group-hover:scale-[0.98] group-hover:opacity-100 transform transition-all ease-out duration-250" />
+          )}
+        </>
+      )}
 
-        {theme === "light" && !disabled && (
-          <div className="hidden lg:block button-grid-bg-section-dark h-full w-full scale-90 opacity-0 group-hover:scale-[1] group-hover:opacity-100 transform transition-all ease-out duration-250" />
-        )}
-        {Icon && <Icon className="w-6 h-6 sm:w-7 sm:h-7 lg:h-8 lg:w-8 text-white" />}
-        {title && <span className="z-20"> {title}</span>}
-      </button>
+      {theme === "light" && !disabled && (
+        <div className="hidden lg:block button-grid-bg-section-dark h-full w-full scale-90 opacity-0 group-hover:scale-[1] group-hover:opacity-100 transform transition-all ease-out duration-250" />
+      )}
+      {Icon && <Icon className="w-6 h-6 sm:w-7 sm:h-7 lg:h-8 lg:w-8 text-white" />}
+      {title && <span className="z-20"> {title}</span>}
     </Link>
   )
 }
