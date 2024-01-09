2ac24ce640c5f251a7ec645d709052e8cce1e765
vvu060 <vvu060@gmail.com>
2024-01-09 11:56:02 +0530

    refactor desktop search

--- src/theme/DocRoot/Layout/Sidebar/index.tsx
+++ src/theme/DocRoot/Layout/Sidebar/index.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react"
+import React, {useEffect, useRef, useState} from "react"
 import Sidebar from "@theme-original/DocRoot/Layout/Sidebar"
 import Search from "docusaurus-lunr-search/src/theme/SearchBar"
 import {useHistory} from "react-router-dom"
@@ -7,13 +7,18 @@ import PageSearchIcon from "@site/static/icons/basic/page-search.svg"
 import EnterKeyIcon from "@site/static/icons/basic/enter-key.svg"
 import UpDownKeyIcon from "@site/static/icons/basic/up-down-key.svg"
 import EscapeKeyIcon from "@site/static/icons/basic/escape-key.svg"
-import styles from "./Sidebar.module.css"
+import styles from "./styles.module.css"
 
-export default function SidebarWrapper(props) {
+const CustomSearch = () => {
   const [isSearchModalVisible, setIsSearchModalVisible] = useState(false)
   const history = useHistory()
+  const searchInputRef = useRef(null)
   const isBrowser = useIsBrowser()
-  const placeholder = isBrowser && window.navigator.platform.startsWith("Mac") ? "Search ⌘+K" : "Search Ctrl+K"
+  const placeholder = isBrowser
+    ? window.navigator.userAgent.startsWith("Mac")
+      ? "Search ⌘+K"
+      : "Search Ctrl+K"
+    : "Search"
 
   function handleSearchClick() {
     setIsSearchModalVisible(true)
@@ -39,12 +44,18 @@ export default function SidebarWrapper(props) {
       handleSearchModalClose()
     }
     if (
-      (event.metaKey && event.key === "k" && navigator.platform.includes("Mac")) ||
-      (event.ctrlKey && event.key === "k" && navigator.platform.includes("Win"))
+      (event.metaKey && event.key === "k" && navigator.userAgent.includes("Mac")) ||
+      (event.ctrlKey && event.key === "k" && navigator.userAgent.includes("Win"))
     ) {
       handleSearchClick()
     }
   }
+
+  useEffect(() => {
+    if (isSearchModalVisible && searchInputRef.current) {
+      searchInputRef.current.focus()
+    }
+  }, [isSearchModalVisible])
 
   useEffect(() => {
     document.addEventListener("keydown", handleKeyPress)
@@ -66,37 +77,18 @@ export default function SidebarWrapper(props) {
     }
   }, [history])
 
-  useEffect(() => {
-    const handleKeyPress = (event) => {
-      if (
-        (event.metaKey && event.key === "k" && navigator.platform.includes("Mac")) ||
-        (event.ctrlKey && event.key === "k" && navigator.platform.includes("Win"))
-      ) {
-        // Your action when CMD/CTRL + K is pressed
-        console.log("CMD/CTRL + K pressed")
-        // Perform your desired action here
-      }
-    }
-
-    document.addEventListener("keydown", handleKeyPress)
-
-    return () => {
-      document.removeEventListener("keydown", handleKeyPress)
-    }
-  }, [])
-
   return (
-    <div className="sidebar-search-container flex flex-col">
+    <>
       <div className={styles.inputContainer} onClick={handleSearchClick}>
         <span aria-label="expand searchbar" role="button" className="search-icon" tabIndex={0}></span>
-        <input readOnly placeholder={placeholder} className={styles.input} />
+        <input readOnly placeholder={placeholder} className={styles.input} ref={searchInputRef} />
       </div>
       {isSearchModalVisible ? (
         <>
           <div onClick={handleSearchModalClose} className={styles.overlay}></div>
           <div className={styles.modal}>
             <div className={styles.modalContent}>
-              <Search />
+              <Search ref={searchInputRef} />
               <div className={styles.initialCase}>
                 <PageSearchIcon />
                 <div className="mt-2 font-bold">Search Docs</div>
@@ -120,6 +112,14 @@ export default function SidebarWrapper(props) {
           </div>
         </>
       ) : null}
+    </>
+  )
+}
+
+export default function SidebarWrapper(props) {
+  return (
+    <div className="sidebar-search-container flex flex-col">
+      <CustomSearch />
       <Sidebar {...props} />
     </div>
   )
