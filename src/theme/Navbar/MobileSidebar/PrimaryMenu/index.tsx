2ba6985855011f95298550c38fda9dc6ccc64b71
vvu060 <vvu060@gmail.com>
2024-01-09 17:20:29 +0530

    Refactor mobile primary menu and add analytics event handler

--- src/theme/Navbar/MobileSidebar/PrimaryMenu/index.tsx
+++ src/theme/Navbar/MobileSidebar/PrimaryMenu/index.tsx
@@ -3,22 +3,35 @@ import {useThemeConfig} from "@docusaurus/theme-common"
 import {useNavbarMobileSidebar} from "@docusaurus/theme-common/internal"
 import NavbarItem from "@theme/NavbarItem"
 import GithubStarsButton from "@site/src/components/shared/GithubStarsButton"
+
+// Function to retrieve navbar items from the theme configuration
 function useNavbarItems() {
-  // TODO temporary casting until ThemeConfig type is improved
+  // TODO: temporary casting until ThemeConfig type is improved
   return useThemeConfig().navbar.items
 }
-// The primary menu displays the navbar items
+
+// Component for the mobile version of the primary navigation menu
 export default function NavbarMobilePrimaryMenu() {
   const mobileSidebar = useNavbarMobileSidebar()
-  // TODO how can the order be defined for mobile?
+  // TODO: How can the order be defined for mobile?
   // Should we allow providing a different list of items?
+
+  // Get the navbar items using the custom hook
   const items = useNavbarItems()
+
   return (
     <ul className="menu__list">
       {items.map((item, i) => (
-        <NavbarItem mobile {...(item as any)} onClick={() => mobileSidebar.toggle()} key={i} />
+        // Render NavbarItem components based on navbar items
+        <NavbarItem
+          mobile
+          {...(item as any)} // Temporarily casting until the type is improved
+          onClick={() => mobileSidebar.toggle()}
+          key={i}
+        />
       ))}
       <li className="menu__list-item">
+        {/* Render the GithubStarsButton component */}
         <GithubStarsButton className="menu__link" />
       </li>
     </ul>
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,8 @@
 import ReactGA from "react-ga4"
 
+// Function to handle analytics events
 export const analyticsHandler = (category: string, action: string, label: string): void => {
+  // Sending event to Google Analytics
   ReactGA.event({
     category: category,
     action: action,
