ac8cd1f54b9ee07d6a21b2e8570b07ff90564be8
vvu060 <vvu060@gmail.com>
2024-01-11 15:59:13 +0530

    Refactor GithubStarsButton component and GithubStarsProvider context

--- src/components/shared/GithubStarsProvider.tsx
+++ src/components/shared/GithubStarsProvider.tsx
@@ -1,16 +1,21 @@
-import React, {createContext, useEffect, useState} from "react"
+import React, {createContext, useEffect, useState, ReactNode} from "react"
 
-export const GithubStarsContext = createContext(null)
+// Define the context type
+type GithubStarsContextType = number | null
 
+// Create the context
+export const GithubStarsContext = createContext<GithubStarsContextType>(null)
+
+// Storage utility to get and set data
 const storage = {
-  get(key) {
+  get(key: string) {
     if (typeof window !== "undefined" && window["__tc_data__"]) {
       return window["__tc_data__"][key]
     }
     return null
   },
 
-  set(key, val) {
+  set(key: string, val: any) {
     if (typeof window !== "undefined") {
       window["__tc_data__"] = window["__tc_data__"] || {}
       window["__tc_data__"][key] = val
@@ -18,14 +23,17 @@ const storage = {
   },
 }
 
-export default function GithubStarsProvider({children}) {
-  const [starsCount, setStarsCount] = useState(storage.get("githubStars"))
+type GithubStarsProviderProps = {
+  children: ReactNode
+}
 
+export default function GithubStarsProvider({children}: GithubStarsProviderProps) {
+  const [starsCount, setStarsCount] = useState<number | null>(storage.get("githubStars"))
+
+  // Fetch Github stars count
   function fetchGithubStars() {
     return fetch("https://api.github.com/repos/tailcallhq/tailcall")
-      .then((resp) => {
-        return resp.json()
-      })
+      .then((resp) => resp.json())
       .then((resp) => {
         const respStarsCount = resp.stargazers_count || "250"
         setStarsCount(respStarsCount)
@@ -34,9 +42,11 @@ export default function GithubStarsProvider({children}) {
       })
   }
 
+  // Fetch stars count on component mount
   useEffect(() => {
     fetchGithubStars()
   }, [])
 
+  // Provide the stars count value through context
   return <GithubStarsContext.Provider value={starsCount}>{children}</GithubStarsContext.Provider>
 }
