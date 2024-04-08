dd6a2fb6d435013c72225928fb9e9a9eb5cf9e88
amit <amitksingh1490@gmail.com>
2024-04-08 16:57:06 +0530

    style: prettier

--- src/theme/DocItem/Layout/index.tsx
+++ src/theme/DocItem/Layout/index.tsx
@@ -1,40 +1,37 @@
-import React from 'react';
-import clsx from 'clsx';
-import { useWindowSize } from '@docusaurus/theme-common';
-import { useDoc } from '@docusaurus/theme-common/internal';
-import DocItemPaginator from '@theme/DocItem/Paginator';
-import DocVersionBanner from '@theme/DocVersionBanner';
-import DocVersionBadge from '@theme/DocVersionBadge';
-import DocItemFooter from '@theme/DocItem/Footer';
-import DocItemTOCMobile from '@theme/DocItem/TOC/Mobile';
-import DocItemTOCDesktop from '@theme/DocItem/TOC/Desktop';
-import DocItemContent from '@theme/DocItem/Content';
-import DocBreadcrumbs from '@theme/DocBreadcrumbs';
-import styles from './styles.module.css';
-import Giscus from '@giscus/react';
-import { useColorMode } from '@docusaurus/theme-common';
+import React from "react"
+import clsx from "clsx"
+import {useWindowSize} from "@docusaurus/theme-common"
+import {useDoc} from "@docusaurus/theme-common/internal"
+import DocItemPaginator from "@theme/DocItem/Paginator"
+import DocVersionBanner from "@theme/DocVersionBanner"
+import DocVersionBadge from "@theme/DocVersionBadge"
+import DocItemFooter from "@theme/DocItem/Footer"
+import DocItemTOCMobile from "@theme/DocItem/TOC/Mobile"
+import DocItemTOCDesktop from "@theme/DocItem/TOC/Desktop"
+import DocItemContent from "@theme/DocItem/Content"
+import DocBreadcrumbs from "@theme/DocBreadcrumbs"
+import styles from "./styles.module.css"
+import Giscus from "@giscus/react"
+import {useColorMode} from "@docusaurus/theme-common"
 /**
  * Decide if the toc should be rendered, on mobile or desktop viewports
  */
 function useDocTOC() {
-  const { frontMatter, toc } = useDoc();
-  const windowSize = useWindowSize();
-  const hidden = frontMatter.hide_table_of_contents;
-  const canRender = !hidden && toc.length > 0;
-  const mobile = canRender ? <DocItemTOCMobile /> : undefined;
-  const desktop =
-    canRender && (windowSize === 'desktop' || windowSize === 'ssr') ? (
-      <DocItemTOCDesktop />
-    ) : undefined;
+  const {frontMatter, toc} = useDoc()
+  const windowSize = useWindowSize()
+  const hidden = frontMatter.hide_table_of_contents
+  const canRender = !hidden && toc.length > 0
+  const mobile = canRender ? <DocItemTOCMobile /> : undefined
+  const desktop = canRender && (windowSize === "desktop" || windowSize === "ssr") ? <DocItemTOCDesktop /> : undefined
   return {
     hidden,
     mobile,
     desktop,
-  };
+  }
 }
-export default function DocItemLayout({ children }) {
-  const docTOC = useDocTOC();
-  const { colorMode } = useColorMode();
+export default function DocItemLayout({children}) {
+  const docTOC = useDocTOC()
+  const {colorMode} = useColorMode()
   const giscus = (
     <React.Fragment>
       <hr />
@@ -58,7 +55,7 @@ export default function DocItemLayout({ children }) {
   )
   return (
     <div className="row">
-      <div className={clsx('col', !docTOC.hidden && styles.docItemCol)}>
+      <div className={clsx("col", !docTOC.hidden && styles.docItemCol)}>
         <DocVersionBanner />
         <div className={styles.docItemContainer}>
           <article>
@@ -74,5 +71,5 @@ export default function DocItemLayout({ children }) {
       </div>
       {docTOC.desktop && <div className="col col--3">{docTOC.desktop}</div>}
     </div>
-  );
+  )
 }
