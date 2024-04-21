c77151ab0c920bdee1c2f576698e2f65709c9368
Mehul Mathur <64700961+mehulmathur16@users.noreply.github.com>
2024-04-21 06:14:26 +0000

    build pass

--- src/components/playground/Playground.tsx
+++ src/components/playground/Playground.tsx
@@ -23,7 +23,7 @@ const useDebouncedValue = (inputValue: string, delay: number) => {
 };
 
 const Playground: React.FC<PlaygroundProps> = ({defaultApiEndpoint}) => {
-    const initialApiEndpoint = new URLSearchParams(window.location.search).get('apiEndpoint') || defaultApiEndpoint;
+    const initialApiEndpoint = typeof window !== 'undefined' && new URLSearchParams(window.location.search).get('apiEndpoint') || defaultApiEndpoint;
     const [apiEndpoint, setApiEndpoint] = useState(initialApiEndpoint);
     const [inputValue, setInputValue] = useState(initialApiEndpoint);
 
@@ -31,32 +31,35 @@ const Playground: React.FC<PlaygroundProps> = ({defaultApiEndpoint}) => {
     const apiEndpointInputClasses = `border border-solid border-tailCall-border-light-500 rounded-lg font-space-grotesk h-11 w-[100%]
     p-SPACE_04 text-content-small outline-none focus:border-x-tailCall-light-700`;
 
-    const fetcher = createGraphiQLFetcher({
-        url: apiEndpoint,
-    });
-
     useEffect(() => {
         setApiEndpoint(debouncedApiEndpoint);
-        // Update URL query parameter
-        const url = new URL(window.location.href);
-        url.searchParams.set('apiEndpoint', debouncedApiEndpoint);
-        window.history.replaceState(null, '', url.toString());
+
+        if(typeof window !== 'undefined') {
+            // Update URL query parameter
+            const url = new URL(window.location.href);
+            url.searchParams.set('apiEndpoint', debouncedApiEndpoint);
+            window.history.replaceState(null, '', url.toString());
+        }
     }, [debouncedApiEndpoint]);
 
     return (
-        <div className='mt-SPACE_06'>
-            <div className='flex px-SPACE_04'>
-                <input
-                name="api-endpoint"
-                type="url"
-                value={inputValue}
-                onChange={(e) => setInputValue(e.target.value)}
-                className={apiEndpointInputClasses}
-                placeholder="API Endpoint"
-                />
-            </div>
-            <GraphiQL fetcher={fetcher} />
-        </div>
+        <>
+            {typeof window !== 'undefined' && 
+                <div className='mt-SPACE_06'>
+                    <div className='flex px-SPACE_04'>
+                        <input
+                        name="api-endpoint"
+                        type="url"
+                        value={inputValue}
+                        onChange={(e) => setInputValue(e.target.value)}
+                        className={apiEndpointInputClasses}
+                        placeholder="API Endpoint"
+                        />
+                    </div>
+                    <GraphiQL fetcher={createGraphiQLFetcher({ url: apiEndpoint })} />
+                </div>
+            }
+        </>
     )
 }
 
