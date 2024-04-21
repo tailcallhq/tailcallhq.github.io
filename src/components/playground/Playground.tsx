06982938cae24339475fb0f3e3022b193c74b08d
Mehul Mathur <64700961+mehulmathur16@users.noreply.github.com>
2024-04-21 06:26:32 +0000

    formatting fix

--- src/components/playground/Playground.tsx
+++ src/components/playground/Playground.tsx
@@ -1,66 +1,68 @@
-import React, { useEffect, useState } from 'react';
-import { createGraphiQLFetcher } from "@graphiql/toolkit";
-import { GraphiQL } from "graphiql";
+import React, {useEffect, useState} from "react"
+import {createGraphiQLFetcher} from "@graphiql/toolkit"
+import {GraphiQL} from "graphiql"
 
 type PlaygroundProps = {
-    defaultApiEndpoint: string;
+  defaultApiEndpoint: string
 }
 
 const useDebouncedValue = (inputValue: string, delay: number) => {
-    const [debouncedValue, setDebouncedValue] = useState(inputValue);
-  
-    useEffect(() => {
-      const handler = setTimeout(() => {
-        setDebouncedValue(inputValue);
-      }, delay);
-  
-      return () => {
-        clearTimeout(handler);
-      };
-    }, [inputValue, delay]);
-  
-    return debouncedValue;
-};
+  const [debouncedValue, setDebouncedValue] = useState(inputValue)
 
-const Playground: React.FC<PlaygroundProps> = ({defaultApiEndpoint}) => {
-    const initialApiEndpoint = typeof window !== 'undefined' && new URLSearchParams(window.location.search).get('apiEndpoint') || defaultApiEndpoint;
-    const [apiEndpoint, setApiEndpoint] = useState(initialApiEndpoint);
-    const [inputValue, setInputValue] = useState(initialApiEndpoint);
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setDebouncedValue(inputValue)
+    }, delay)
 
-    const debouncedApiEndpoint = useDebouncedValue(inputValue, 500);
-    const apiEndpointInputClasses = `border border-solid border-tailCall-border-light-500 rounded-lg font-space-grotesk h-11 w-[100%]
-    p-SPACE_04 text-content-small outline-none focus:border-x-tailCall-light-700`;
+    return () => {
+      clearTimeout(handler)
+    }
+  }, [inputValue, delay])
 
-    useEffect(() => {
-        setApiEndpoint(debouncedApiEndpoint);
-
-        if(typeof window !== 'undefined') {
-            // Update URL query parameter
-            const url = new URL(window.location.href);
-            url.searchParams.set('apiEndpoint', debouncedApiEndpoint);
-            window.history.replaceState(null, '', url.toString());
-        }
-    }, [debouncedApiEndpoint]);
-
-    return (
-        <>
-            {typeof window !== 'undefined' && 
-                <div className='mt-SPACE_06'>
-                    <div className='flex px-SPACE_04'>
-                        <input
-                        name="api-endpoint"
-                        type="url"
-                        value={inputValue}
-                        onChange={(e) => setInputValue(e.target.value)}
-                        className={apiEndpointInputClasses}
-                        placeholder="API Endpoint"
-                        />
-                    </div>
-                    <GraphiQL fetcher={createGraphiQLFetcher({ url: apiEndpoint })} />
-                </div>
-            }
-        </>
-    )
+  return debouncedValue
 }
 
-export default Playground;
+const Playground: React.FC<PlaygroundProps> = ({defaultApiEndpoint}) => {
+  const initialApiEndpoint =
+    (typeof window !== "undefined" && new URLSearchParams(window.location.search).get("apiEndpoint")) ||
+    defaultApiEndpoint
+  const [apiEndpoint, setApiEndpoint] = useState(initialApiEndpoint)
+  const [inputValue, setInputValue] = useState(initialApiEndpoint)
+
+  const debouncedApiEndpoint = useDebouncedValue(inputValue, 500)
+  const apiEndpointInputClasses = `border border-solid border-tailCall-border-light-500 rounded-lg font-space-grotesk h-11 w-[100%]
+    p-SPACE_04 text-content-small outline-none focus:border-x-tailCall-light-700`
+
+  useEffect(() => {
+    setApiEndpoint(debouncedApiEndpoint)
+
+    if (typeof window !== "undefined") {
+      // Update URL query parameter
+      const url = new URL(window.location.href)
+      url.searchParams.set("apiEndpoint", debouncedApiEndpoint)
+      window.history.replaceState(null, "", url.toString())
+    }
+  }, [debouncedApiEndpoint])
+
+  return (
+    <>
+      {typeof window !== "undefined" && (
+        <div className="mt-SPACE_06">
+          <div className="flex px-SPACE_04">
+            <input
+              name="api-endpoint"
+              type="url"
+              value={inputValue}
+              onChange={(e) => setInputValue(e.target.value)}
+              className={apiEndpointInputClasses}
+              placeholder="API Endpoint"
+            />
+          </div>
+          <GraphiQL fetcher={createGraphiQLFetcher({url: apiEndpoint})} />
+        </div>
+      )}
+    </>
+  )
+}
+
+export default Playground
--- src/pages/playground.tsx
+++ src/pages/playground.tsx
@@ -1,14 +1,14 @@
-import React from "react";
+import React from "react"
 import Layout from "@theme/Layout"
-import "graphiql/graphiql.css";
-import Playground from "../components/playground/Playground";
+import "graphiql/graphiql.css"
+import Playground from "../components/playground/Playground"
 
 const PlaygroundPage = () => {
-    return (
-        <Layout title="Tailcall Playground" description="Explore, test, and interact with your GraphQL APIs effortlessly">
-            <Playground defaultApiEndpoint="http://localhost:8000" />
-        </Layout>
-    );
+  return (
+    <Layout title="Tailcall Playground" description="Explore, test, and interact with your GraphQL APIs effortlessly">
+      <Playground defaultApiEndpoint="http://localhost:8000" />
+    </Layout>
+  )
 }
 
-export default PlaygroundPage;
+export default PlaygroundPage
