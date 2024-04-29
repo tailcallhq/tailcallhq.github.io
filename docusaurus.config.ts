65ab24dd3c4a8be08272c914c1aa8f0281ecc7a3
neo773 <62795688+neo773@users.noreply.github.com>
2024-04-29 19:54:07 +0530

    add temp base url

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -28,7 +28,7 @@ export default {
     },
   ],
   url: "https://tailcall.run",
-  baseUrl: "/",
+  baseUrl: "/tailcallhq.github.io/",
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
   favicon: "images/favicon.ico",
