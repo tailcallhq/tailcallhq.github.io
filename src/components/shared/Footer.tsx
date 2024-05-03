3d87f60715e3d5eea0ef03171703c6dc4c31e31e
Ranjit Mahadik <43403528+ranjitmahadik@users.noreply.github.com>
2024-05-03 23:14:42 +0530

    - added zindex to fix overlap issue.

--- src/components/shared/Footer.tsx
+++ src/components/shared/Footer.tsx
@@ -6,7 +6,7 @@ import {pageLinks} from "@site/src/constants/routes"
 
 const Footer = (): JSX.Element => {
   return (
-    <footer className="bg-tailCall-dark-700 pt-SPACE_10 pb-SPACE_05 flex flex-col items-center justify-center gap-SPACE_10 w-full relative px-SPACE_04">
+    <footer style={{zIndex: 101}} className="bg-tailCall-dark-700 pt-SPACE_10 pb-SPACE_05 flex flex-col items-center justify-center gap-SPACE_10 w-full relative px-SPACE_04">
       <img
         src={require("@site/static/images/about/grid-dark.png").default}
         alt=""
