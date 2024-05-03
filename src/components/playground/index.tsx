ddf0e809e0fd89b95ce86928922269e8f9752caa
Ranjit Mahadik <43403528+ranjitmahadik@users.noreply.github.com>
2024-05-04 00:17:14 +0530

    - remove unused code.

--- src/components/playground/index.tsx
+++ src/components/playground/index.tsx
@@ -1,15 +1,7 @@
-import React, {useEffect, useState} from "react"
+import React from "react"
 import Playground from "./Playground"
 
 const PlaygroundPage = (): JSX.Element => {
-  const [editorLoaded, setEditorLoaded] = useState(false)
-
-  useEffect(() => {
-    if (document.getElementsByClassName("graphiql-container")) {
-      setEditorLoaded(true)
-    }
-  }, [])
-
   return (
       <Playground />
   )
