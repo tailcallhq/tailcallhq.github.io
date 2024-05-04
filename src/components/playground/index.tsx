0bde381dc2428854f9f4bfe86b0549a077f82db8
Ranjit Mahadik <43403528+ranjitmahadik@users.noreply.github.com>
2024-05-04 20:30:01 +0530

    - removed state as it's no longer required. (#240)

--- src/components/playground/index.tsx
+++ src/components/playground/index.tsx
@@ -2,20 +2,7 @@ import React, {useEffect, useState} from "react"
 import Playground from "./Playground"
 
 const PlaygroundPage = (): JSX.Element => {
-  const [editorLoaded, setEditorLoaded] = useState(false)
-
-  useEffect(() => {
-    if (document.getElementsByClassName("graphiql-container")) {
-      setEditorLoaded(true)
-    }
-  }, [])
-
-  return (
-    <>
-      <Playground />
-      {editorLoaded}
-    </>
-  )
+  return <Playground />
 }
 
 export default PlaygroundPage
