7aa824b62f2ef3ce99ece6a3d4487661d0378c21
neo773 <62795688+neo773@users.noreply.github.com>
2024-05-29 04:14:31 +0530

    add contact page

---
+++ marketing/src/app/contact/page.tsx
@@ -0,0 +1,17 @@
+import React from "react"
+import { Metadata } from "next"
+import { PageDescription, PageTitle } from "@/constants/titles"
+import ContactPage from '@/components/contact'
+
+export const metadata: Metadata = {
+  title: PageTitle.CONTACT,
+  description: PageDescription.CONTACT,
+}
+
+const Contact = (): JSX.Element => {
+  return (
+      <ContactPage />
+  )
+}
+
+export default Contact
