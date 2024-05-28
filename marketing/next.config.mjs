4ca17ecac0d7d4613b633f92112b454d18d54851
neo773 <62795688+neo773@users.noreply.github.com>
2024-05-29 01:53:33 +0530

    tweak build command

--- marketing/next.config.mjs
+++ marketing/next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  rewrites: async () => [
+    {
+      source: "/doc",
+      destination: "/doc/index.html",
+    },
+  ],
 }
 
 export default nextConfig
