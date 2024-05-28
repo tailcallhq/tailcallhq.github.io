caab860a270c2cb7201b34d50493269e5d7c5135
neo773 <62795688+neo773@users.noreply.github.com>
2024-05-29 05:09:12 +0530

    fix build

--- marketing/src/app/not-found.tsx
+++ marketing/src/app/not-found.tsx
@@ -4,7 +4,7 @@ export default function NotFound() {
   return (
     <div className="px-8 py-24 flex justify-center items-center">
       <div className="w-1/2">
-        <h1 class="text-[3rem] font-bold">Page Not Found</h1>
+        <h1 className="text-[3rem] font-bold">Page Not Found</h1>
         <p className="my-3">We could not find what you were looking for.</p>
         <p>
           Please contact the owner of the site that linked you to the original URL and let them know their link is
