115ed64725998d71bf5eb96c418c403d18a8ea6d
neo773 <62795688+neo773@users.noreply.github.com>
2024-05-29 05:13:21 +0530

    404 responsice fixes

--- marketing/src/app/not-found.tsx
+++ marketing/src/app/not-found.tsx
@@ -2,9 +2,9 @@ import Link from "next/link"
 
 export default function NotFound() {
   return (
-    <div className="px-8 py-24 flex justify-center items-center">
-      <div className="w-1/2">
-        <h1 className="text-[3rem] font-bold">Page Not Found</h1>
+    <div className="px-4 py-24 flex justify-center items-center">
+      <div className="w-full md:w-1/2">
+        <h1 className="text-[2rem] md:text-[3rem] font-bold">Page Not Found</h1>
         <p className="my-3">We could not find what you were looking for.</p>
         <p>
           Please contact the owner of the site that linked you to the original URL and let them know their link is
