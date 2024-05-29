9b01d72af84f795665a75091f73761573809c16e
neo773 <62795688+neo773@users.noreply.github.com>
2024-05-29 21:58:23 +0530

    add back layout

---
+++ src/theme/Layout/Provider/index.tsx
@@ -0,0 +1,34 @@
+import React, {ReactNode} from "react"
+import {composeProviders} from "@docusaurus/theme-common"
+import {
+  ColorModeProvider,
+  AnnouncementBarProvider,
+  DocsPreferredVersionContextProvider,
+  ScrollControllerProvider,
+  NavbarProvider,
+  PluginHtmlClassNameProvider,
+} from "@docusaurus/theme-common/internal"
+import GithubStarsProvider from "@site/src/components/shared/GithubStarsProvider"
+
+// Define the type for LayoutProvider props
+type LayoutProviderProps = {
+  children: ReactNode // ReactNode represents any React child (JSX, strings, etc.)
+}
+
+// Compose the providers to create a single Provider component
+const Provider = composeProviders([
+  ColorModeProvider,
+  AnnouncementBarProvider,
+  ScrollControllerProvider,
+  DocsPreferredVersionContextProvider,
+  PluginHtmlClassNameProvider,
+  NavbarProvider,
+  GithubStarsProvider,
+])
+
+// LayoutProvider component wraps the composed providers around its children
+const LayoutProvider = ({children}: LayoutProviderProps) => {
+  return <Provider>{children}</Provider>
+}
+
+export default LayoutProvider
