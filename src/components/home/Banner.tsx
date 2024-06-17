698c31e782eb2998257eb8de8931be722688f7ee
Tushar Mathur <tusharmath@gmail.com>
2024-06-17 13:41:51 +0530

    update banner and first line

--- src/components/home/Banner.tsx
+++ src/components/home/Banner.tsx
@@ -17,12 +17,11 @@ const Banner = (): JSX.Element => {
             as="h2"
             className="text-title-large max-w-xs sm:text-display-small lg:text-display-large sm:max-w-5xl"
           >
-            GraphQL platform engineered for{" "}
-            <span className="bg-tailCall-yellow rounded-md sm:rounded-2xl px-SPACE_02">scale</span>
+            The modern <br />
+            <span className="bg-tailCall-yellow rounded-md sm:rounded-2xl px-SPACE_02">GraphQL</span> runtime
           </Heading>
           <p className="sm:max-w-2xl sm:m-auto text-content-small sm:text-content-medium lg:text-content-large font-normal max-w-md sm:mt-SPACE_04 mb-0">
-            Tailcall is a high-performance, open-source API orchestrator that enables organizations to construct unified
-            data access layers on top of existing APIs.
+            Swiftly design and ship best-practice GraphQL backends atop existing data sources and APIs.
           </p>
           <div className="hidden sm:flex justify-center mt-SPACE_06 sm:mt-SPACE_10 space-x-SPACE_04 sm:space-x-SPACE_06">
             <LinkButton
