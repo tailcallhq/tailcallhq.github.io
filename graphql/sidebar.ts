9f755b3d128878643b9ccddfefa7feee76a9c25b
amit <amitksingh1490@gmail.com>
2024-07-03 21:30:39 +0530

    doc(graphql): update sidebar

--- graphql/sidebar.ts
+++ graphql/sidebar.ts
@@ -2,17 +2,16 @@ import {SidebarsConfig} from "@docusaurus/plugin-content-docs"
 
 const sidebars: SidebarsConfig = {
   graphql: [
-    "graphql",
-    "what-is-graphql",
-    "schema-and-types",
-    "queries",
-    "mutations",
-    "variables",
-    "fragments",
-    "introspection",
-    "graphql-vs-rest",
-    "cto-guide",
-    "problem-statement",
+    {
+      type: "category",
+      label: "Guides",
+      items: ["graphql", "graphql-vs-rest", "cto-guide", "problem-statement"],
+    },
+    {
+      type: "category",
+      label: "GraphQL Tutorial",
+      items: ["what-is-graphql", "schema-and-types", "queries", "mutations", "variables", "fragments", "introspection"],
+    },
   ],
 }
 
