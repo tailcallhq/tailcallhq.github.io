1cb661a92a6061d1a4e63f8d092a82a9a3b7268f
neo773 <62795688+neo773@users.noreply.github.com>
2024-07-12 17:37:40 +0530

    hashnode fixes (#337)
    * fixes
    
    * make seo_title optional
    
    * chore: prettier
    
    * add changes
    
    * add changes

--- publish-hashnode/src/utils/constants.ts
+++ publish-hashnode/src/utils/constants.ts
@@ -1,2 +1,10 @@
 export const HASHNODE_PAT = process.env.HASHNODE_PAT!
 export const HASHNODE_PUBLICATION_ID = process.env.HASHNODE_PUBLICATION_ID!
+export const BASE_URL_ASSETS = "https://raw.githubusercontent.com/tailcallhq/tailcallhq.github.io/develop/"
+
+// hashnode author ids
+export const authorsMap = {
+  tushar: "64814d2ae39007f9ec4374fe",
+  amit: "656ad62cbb72ffb16a117322",
+  sujeet: "65135b4fa5cceaee505f5c19",
+}
