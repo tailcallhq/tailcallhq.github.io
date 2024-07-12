33161b77c30839c0b49378b63fdc50aaabbb8b00
neo773 <62795688+neo773@users.noreply.github.com>
2024-07-12 17:01:18 +0530

    chore: prettier

--- publish-hashnode/src/utils/constants.ts
+++ publish-hashnode/src/utils/constants.ts
@@ -4,7 +4,7 @@ export const BASE_URL_ASSETS = "https://raw.githubusercontent.com/tailcallhq/tai
 
 // hashnode author ids
 export const authorsMap = {
-    tushar: "64814d2ae39007f9ec4374fe",
-    amit: "656ad62cbb72ffb16a117322",
-    sujeet: "65135b4fa5cceaee505f5c19",
+  tushar: "64814d2ae39007f9ec4374fe",
+  amit: "656ad62cbb72ffb16a117322",
+  sujeet: "65135b4fa5cceaee505f5c19",
 }
