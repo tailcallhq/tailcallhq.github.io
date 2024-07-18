8e42b9b3796c94eaec33835db7828d7f5b0ef42e
hunxjunedo <junaidhunain6@gmail.com>
2024-07-18 17:40:52 +0500

    fix type errors and touch up some colors

--- src/theme/CodeBlock/index.tsx
+++ src/theme/CodeBlock/index.tsx
@@ -9,9 +9,9 @@ type Props = WrapperProps<typeof CodeBlockType>
 export default function CodeBlockWrapper(props: Props): JSX.Element {
   console.log(props)
   return (
-    <div style={{background: "rgb(58, 65, 79)"}} className=" overflow-clip rounded-xl z-10">
+    <div style={{background: "rgb(44, 52, 68)"}} className=" overflow-clip rounded-2xl z-10">
       <div className="h-fit p-3 flex items-center justify-end">
-        <CopyButton className="mt-3 mr-5 text-gray-300 text-opacity-60 hover:text-opacity-80" code={props.children} />
+        <CopyButton className="mt-3 mr-5 text-gray-300 text-opacity-60 hover:text-opacity-80" code={props.children as string} />
       </div>
       <CodeBlock {...props} />
     </div>
