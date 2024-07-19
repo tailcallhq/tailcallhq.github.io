2e557dc4bffa443e259959e7379dd6a42bac65cd
neo773 <62795688+neo773@users.noreply.github.com>
2024-07-19 14:53:39 +0530

    fix: radius and parse meta string

--- src/theme/CodeBlock/index.tsx
+++ src/theme/CodeBlock/index.tsx
@@ -5,6 +5,13 @@ import type {WrapperProps} from "@docusaurus/types"
 
 type Props = WrapperProps<typeof CodeBlockType>
 
+type Primitive = string | boolean | number
+
+interface MetastringData extends Record<string, Primitive> {
+  title: string
+  showLineNumbers: boolean
+}
+
 export default function CodeBlockWrapper(props: Props): JSX.Element {
   const [copied, setCopied] = useState(false)
 
@@ -14,9 +21,24 @@ export default function CodeBlockWrapper(props: Props): JSX.Element {
     setTimeout(() => setCopied(false), 2000)
   }
 
+  const parseMetastring = <T extends Partial<Record<string, Primitive>>>(metastring: string | undefined): T => {
+    if (!metastring) return {} as T
+    const result = {} as T
+    const regex = /(\w+)(?:="([^"]*)"|\s*)/g
+    let match
+    while ((match = regex.exec(metastring)) !== null) {
+      const [, key, value] = match
+      result[key as keyof T] = (value || "true") as T[keyof T]
+    }
+    return result
+  }
+
+  const metastringData = parseMetastring<MetastringData>(props.metastring)
+
   return (
-    <div className="rounded-lg overflow-hidden">
-      <div className="bg-[#35353A] p-2 flex justify-end">
+    <div className="rounded-3xl overflow-hidden">
+      <div className="bg-[#35353A] p-4 flex justify-between items-center">
+        <span className="text-white text-xs font-space-mono">{metastringData.title}</span>
         <div className="relative">
           <button
             onClick={handleCopy}
