9d621d1ac5fee6a37bea77682df4e2546b98446e
harshtech123 <139060630+harshtech123@users.noreply.github.com>
2024-07-22 18:47:17 +0530

    update

--- tailcall.config.ts
+++ tailcall.config.ts
@@ -1,41 +1,84 @@
 // tailcall.config.ts
 
-schema @server {
-    query: Query
-  }
-  
-  type PostInput {
-    title: String!
-    body: String!
-  }
-  
-  type PostResponse {
-    success: Boolean!
-    message: String
-  }
-  
-  type Query {
-    publishPostToHashnode(input: PostInput!): PostResponse @http(
-      method: "POST",
-      url: "https://api.hashnode.com",
-      headers: { "Content-Type": "application/json" },
-      body: { query: "mutation { createStory(input: { title: \"{{.input.title}}\", contentMarkdown: \"{{.input.body}}\" }) { success message } }" }
-    )
-    publishPostToDevTo(input: PostInput!): PostResponse @http(
-      method: "POST",
-      url: "https://dev.to/api/articles",
-      headers: {
-        "Content-Type": "application/json",
-        "api-key": "{{devToApiKey}}"
-      },
-      body: { article: { title: "{{.input.title}}", body_markdown: "{{.input.body}}" } }
-    )
-  }
-  
-  type Mutation {
-    publishPost(input: PostInput!): PostResponse @call(
-      call: "publishPostToHashnode(input: {{.input}})",
-      call: "publishPostToDevTo(input: {{.input}})"
-    )
-  }
-  
+import { defineConfig } from "@tailcallhq/tailcall";
+
+export default defineConfig({
+  schema: `
+    schema @server @upstream(baseURL: "https://jsonplaceholder.typicode.com") {
+      query: Query
+    }
+
+    type Address {
+      city: String
+      geo: Geo
+      street: String
+      suite: String
+      zipcode: String
+    }
+
+    type Company {
+      bs: String
+      catchPhrase: String
+      name: String
+    }
+
+    type Geo {
+      lat: String
+      lng: String
+    }
+
+    type Photo {
+      albumId: Int
+      id: Int
+      thumbnailUrl: String
+      title: String
+      url: String
+    }
+
+    type Post {
+      body: String
+      id: Int
+      title: String
+      userId: Int
+    }
+
+    type Comment {
+      body: String
+      email: String
+      id: Int
+      name: String
+      postId: Int
+    }
+
+    type Query {
+      comment(id: Int!): Comment @http(path: "/comments/{{.args.id}}")
+      comments: [Comment] @http(path: "/comments")
+      photo(id: Int!): Photo @http(path: "/photos/{{.args.id}}")
+      photos: [Photo] @http(path: "/photos")
+      post(id: Int!): Post @http(path: "/posts/{{.args.id}}")
+      posts: [Post] @http(path: "/posts")
+      todo(id: Int!): Todo @http(path: "/todos/{{.args.id}}")
+      todos: [Todo] @http(path: "/todos")
+      user(id: Int!): User @http(path: "/users/{{.args.id}}")
+      users: [User] @http(path: "/users")
+    }
+
+    type Todo {
+      completed: Boolean
+      id: Int
+      title: String
+      userId: Int
+    }
+
+    type User {
+      address: Address
+      company: Company
+      email: String
+      id: Int
+      name: String
+      phone: String
+      username: String
+      website: String
+    }
+  `
+});
