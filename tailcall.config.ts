133fedaa2187fa0ccc95f0ae2a016a5c7093ef72
harshtech123 <139060630+harshtech123@users.noreply.github.com>
2024-07-22 19:15:21 +0530

    Update tailcall.config.ts

--- tailcall.config.ts
+++ tailcall.config.ts
@@ -1,84 +1,84 @@
-// tailcall.config.ts
-
-import { defineConfig } from "@tailcallhq/tailcall";
+import {defineConfig} from "@tailcallhq/tailcall"
 
 export default defineConfig({
-  schema: `
-    schema @server @upstream(baseURL: "https://jsonplaceholder.typicode.com") {
-      query: Query
-    }
+  schema: {
+    typeDefs: `
+      schema @server @upstream(baseURL: "https://jsonplaceholder.typicode.com") {
+        query: Query
+      }
 
-    type Address {
-      city: String
-      geo: Geo
-      street: String
-      suite: String
-      zipcode: String
-    }
+      type Address {
+        city: String
+        geo: Geo
+        street: String
+        suite: String
+        zipcode: String
+      }
 
-    type Company {
-      bs: String
-      catchPhrase: String
-      name: String
-    }
+      type Company {
+        bs: String
+        catchPhrase: String
+        name: String
+      }
 
-    type Geo {
-      lat: String
-      lng: String
-    }
+      type Geo {
+        lat: String
+        lng: String
+      }
 
-    type Photo {
-      albumId: Int
-      id: Int
-      thumbnailUrl: String
-      title: String
-      url: String
-    }
+      type Photo {
+        albumId: Int
+        id: Int
+        thumbnailUrl: String
+        title: String
+        url: String
+      }
 
-    type Post {
-      body: String
-      id: Int
-      title: String
-      userId: Int
-    }
+      type Post {
+        body: String
+        id: Int
+        title: String
+        userId: Int
+      }
 
-    type Comment {
-      body: String
-      email: String
-      id: Int
-      name: String
-      postId: Int
-    }
+      type Comment {
+        body: String
+        email: String
+        id: Int
+        name: String
+        postId: Int
+      }
 
-    type Query {
-      comment(id: Int!): Comment @http(path: "/comments/{{.args.id}}")
-      comments: [Comment] @http(path: "/comments")
-      photo(id: Int!): Photo @http(path: "/photos/{{.args.id}}")
-      photos: [Photo] @http(path: "/photos")
-      post(id: Int!): Post @http(path: "/posts/{{.args.id}}")
-      posts: [Post] @http(path: "/posts")
-      todo(id: Int!): Todo @http(path: "/todos/{{.args.id}}")
-      todos: [Todo] @http(path: "/todos")
-      user(id: Int!): User @http(path: "/users/{{.args.id}}")
-      users: [User] @http(path: "/users")
-    }
+      type Query {
+        comment(id: Int!): Comment @http(path: "/comments/{{.args.id}}")
+        comments: [Comment] @http(path: "/comments")
+        photo(id: Int!): Photo @http(path: "/photos/{{.args.id}}")
+        photos: [Photo] @http(path: "/photos")
+        post(id: Int!): Post @http(path: "/posts/{{.args.id}}")
+        posts: [Post] @http(path: "/posts")
+        todo(id: Int!): Todo @http(path: "/todos/{{.args.id}}")
+        todos: [Todo] @http(path: "/todos")
+        user(id: Int!): User @http(path: "/users/{{.args.id}}")
+        users: [User] @http(path: "/users")
+      }
 
-    type Todo {
-      completed: Boolean
-      id: Int
-      title: String
-      userId: Int
-    }
+      type Todo {
+        completed: Boolean
+        id: Int
+        title: String
+        userId: Int
+      }
 
-    type User {
-      address: Address
-      company: Company
-      email: String
-      id: Int
-      name: String
-      phone: String
-      username: String
-      website: String
-    }
-  `
-});
+      type User {
+        address: Address
+        company: Company
+        email: String
+        id: Int
+        name: String
+        phone: String
+        username: String
+        website: String
+      }
+    `,
+  },
+})
