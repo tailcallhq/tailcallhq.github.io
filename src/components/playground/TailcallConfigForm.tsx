1cb04e4fca49d5f1d574a48920181c3670195378
mehulmathur001 <160306774+mehulmathur001@users.noreply.github.com>
2024-07-23 15:28:51 +0000

    chore: form buttons

--- src/components/playground/TailcallConfigForm.tsx
+++ src/components/playground/TailcallConfigForm.tsx
@@ -1,11 +1,13 @@
-import React, {useEffect, useState} from "react"
+import React, { useEffect, useState } from "react"
 import Form from "@rjsf/mui"
 import jsYaml from "js-yaml"
 import axios from "axios"
-import {configFileName, tailcallConfigSchema} from "@site/src/constants"
+import { configFileName, tailcallConfigSchema } from "@site/src/constants"
+import { Theme } from "@site/src/constants"
 import validator from "@rjsf/validator-ajv8"
 import "../../css/configForm.css"
-import {downloadFile} from "@site/src/utils"
+import { downloadFile } from "@site/src/utils"
+import LinkButton from "../shared/LinkButton"
 
 const formContext = {
   className: "font-space-grotesk-imp",
@@ -13,23 +15,26 @@ const formContext = {
 
 const TailcallConfigForm = () => {
   const [schema, setSchema] = useState({})
+  const [formData, setFormData] = useState({})
 
   useEffect(() => {
     axios.get(`${tailcallConfigSchema}?v=${+new Date()}`).then((res) => setSchema(res.data))
   }, [])
 
-  const handleSubmit = ({formData}: any) => {
+  const handleSubmit = () => {
     console.log("Data submitted: ", formData)
-    // downloadConfigJson(formData)
-    // downloadConfigYaml(formData)
   }
 
-  const downloadConfigJson = (formData: any) => {
+  const onFormDataChange = ({ formData }: any) => {
+    setFormData(formData)
+  }
+
+  const downloadConfigJson = () => {
     const jsonString = JSON.stringify(formData, null, 2)
     downloadFile(jsonString, "application/json", `${configFileName}.json`)
   }
 
-  const downloadConfigYaml = (formData: any) => {
+  const downloadConfigYaml = () => {
     const yamlString = jsYaml.dump(formData)
     downloadFile(yamlString, "text/yaml", `${configFileName}.yml`)
   }
@@ -39,12 +44,29 @@ const TailcallConfigForm = () => {
       <Form
         className="config-form"
         schema={schema}
+        formData={formData}
+        onChange={onFormDataChange}
         formContext={formContext}
         validator={validator}
-        onSubmit={handleSubmit}
         focusOnFirstError
         liveValidate
-      />
+      >
+        <div className="flex items-center justify-center">
+          <button type='submit' onClick={handleSubmit} className="bg-white border-none">
+            <LinkButton theme={Theme.Dark} title="Submit" />
+          </button>
+          {/* {isConfigValid && (
+            <> */}
+          <button type="button" onClick={downloadConfigJson} className="bg-white border-none">
+            <LinkButton theme={Theme.Dark} title="Download JSON Config" />
+          </button>
+          <button type="button" onClick={downloadConfigYaml} className="bg-white border-none">
+            <LinkButton theme={Theme.Dark} title="Download YAML Config" />
+          </button>
+          {/* </>
+          )} */}
+        </div>
+      </Form>
     </div>
   )
 }
