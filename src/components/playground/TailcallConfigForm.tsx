ec0dae2406d4c588d469c9a6dc7ec24400fea4a3
Bhavya Jain <rishikabhavya@gmail.com>
2024-07-24 15:13:26 +0530

    Heading updates

--- src/components/playground/TailcallConfigForm.tsx
+++ src/components/playground/TailcallConfigForm.tsx
@@ -1,17 +1,17 @@
-import React, {useEffect, useState} from "react"
+import React, { useEffect, useState } from "react"
 import Form from "@rjsf/mui"
 import jsYaml from "js-yaml"
 import axios from "axios"
-import {configFileName, tailcallConfigSchema} from "@site/src/constants"
+import { configFileName, tailcallConfigSchema } from "@site/src/constants"
 import validator from "@rjsf/validator-ajv8"
 import "../../css/configForm.css"
-import {downloadFile} from "@site/src/utils"
+import { downloadFile } from "@site/src/utils"
 import Modal from "@mui/material/Modal"
 import Box from "@mui/material/Box"
 import FieldTemplate from "./FieldTemplate" // Import your custom FieldTemplate
-import {DescriptionFieldProps, TitleFieldProps, UiSchema} from "@rjsf/utils"
-import {Download, Close} from "@mui/icons-material"
-import {IconButton} from "@mui/material"
+import { DescriptionFieldProps, TitleFieldProps, UiSchema } from "@rjsf/utils"
+import { Download, Close } from "@mui/icons-material"
+import { IconButton } from "@mui/material"
 
 const formContext = {
   className: "font-space-grotesk-imp",
@@ -27,15 +27,19 @@ const TailcallConfigForm = () => {
     axios.get(`${tailcallConfigSchema}?v=${+new Date()}`).then((res) => setSchema(res.data))
   }, [])
 
-  const handleSubmit = ({formData}: any) => {
+  const handleSubmit = ({ formData }: any) => {
     console.log("Data submitted: ", formData)
     setIsFormSubmitted(true)
     setIsModalOpen(true)
   }
-  
-  const onFormDataChange = ({formData}: any) => {
+
+  const onFormDataChange = ({ formData }: any) => {
     setFormData(formData)
     setIsFormSubmitted(false)
+  }
+
+  const uiSchema: UiSchema = {
+    "ui:title": "Generate Tailcall Configuration"
   }
 
   const downloadConfigJson = () => {
@@ -57,7 +61,7 @@ const TailcallConfigForm = () => {
     setIsModalOpen(false)
   }
 
-  const DownloadButton = ({onClick, title}: {onClick: () => void; title: string}) => (
+  const DownloadButton = ({ onClick, title }: { onClick: () => void; title: string }) => (
     <button
       type="button"
       onClick={onClick}
@@ -68,10 +72,6 @@ const TailcallConfigForm = () => {
       {title}
     </button>
   )
-
-  function TitleFieldTemplate() {
-    return null
-  }
 
   function DescriptionFieldTemplate(props: DescriptionFieldProps) {
     console.log(props)
@@ -90,8 +90,8 @@ const TailcallConfigForm = () => {
         formContext={formContext}
         validator={validator}
         focusOnFirstError
-        // liveValidate
-        templates={{DescriptionFieldTemplate}} // Use the custom FieldTemplate
+        uiSchema={uiSchema}
+        templates={{ DescriptionFieldTemplate }} // Use the custom templates
       >
         <div className="flex items-center justify-center">
           <button type="submit" className="border-none py-3 px-8 rounded-md bg-black text-white">
