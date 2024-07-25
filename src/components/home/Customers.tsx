04f91170d578d5194952f8fa4cde2dc9790b7325
mehulmathur001 <160306774+mehulmathur001@users.noreply.github.com>
2024-07-25 16:30:53 +0000

    styles fix

--- src/components/home/Customers.tsx
+++ src/components/home/Customers.tsx
@@ -16,7 +16,7 @@ const Customer = () => {
                         <span className='bg-tailCall-yellow rounded-lg text-black px-SPACE_01 ml-SPACE_02'>love us!</span>
                     </Heading>
                 </div>
-                <div className='flex flex-col space-y-SPACE_06 md:flex-row md:space-x-SPACE_02'>
+                <div className='flex flex-col space-y-SPACE_06 md:flex-row md:space-x-SPACE_02 md:space-y-0'>
                     {
                         customerFeedbacks.map((feedback) => {
                             return (
