71b60fe5f86752e767a0d3d0fec5c2cf4f078a5f
Bhavya Jain <rishikabhavya@gmail.com>
2024-07-25 21:20:22 +0530

    Mobile View chnages

--- src/components/home/Benefits.tsx
+++ src/components/home/Benefits.tsx
@@ -10,13 +10,13 @@ import { ArrowBigRight, ArrowRight } from "lucide-react"
 
 const LegacyGateway = (): JSX.Element => {
   return (
-    <section className="w-full pt-10 lg:px-SPACE_16 bg-[#1C1D1F]">
+    <section className="w-full md:pt-10 p-10 lg:px-SPACE_16 bg-[#1C1D1F]">
       <div>
         <SectionTitle title="Benefits" />
         <div className=" h-fit sm:flex-row sm:items-center sm:space-x-SPACE_10 lg:space-x-SPACE_20">
           <Heading
             as="h3"
-            className="text-title-large sm:text-display-tiny lg:text-display-small text-white w-[65%]"
+            className="text-title-large sm:text-display-tiny lg:text-display-small text-white md:w-[65%]"
           >
             Tailcall gives you a best-practice GraphQL backend that checks all the boxes.
           </Heading>
@@ -25,14 +25,14 @@ const LegacyGateway = (): JSX.Element => {
       <div className="flex w-[100%] flex-wrap items-center justify-center">
         {benefits.map((item) => (
           <div
-            className="border border-solid border-tailCall-border-dark-300 w-[40%]  rounded-3xl px-10 py-2 flex items-center justify-between m-4 hover:border-[#FDEA2E]"
+            className="border border-solid border-tailCall-border-dark-300 md:w-[40%]  rounded-3xl md:px-10 px-4 py-2 md:flex items-center justify-between md:m-4 mb-10 hover:border-[#FDEA2E]"
             key={item.id}
           >
             <div className="h-16 w-16 sm:w-full sm:h-full">
               <img src={item.image} alt="Image Describing Why Tailcall" className="max-w-[72px] sm:max-w-[110px]" />
             </div>
             <div>
-              <p className="text-title-small text-white sm:text-title-large mt-SPACE_04 mb-SPACE_02 sm:mb-0">{item.title} <span className="h-16 w-16 "><ArrowRight/></span></p>
+              <p className="text-title-small text-white sm:text-title-large mt-SPACE_04 mb-SPACE_02 sm:mb-0">{item.title} <span className="h-16 w-16"><ArrowRight/></span></p>
               <p className="text-content-tiny sm:text-content-small text-tailCall-light-600">{item.description}</p>
             </div>
           </div>
