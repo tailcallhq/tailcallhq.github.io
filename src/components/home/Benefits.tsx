4c53974056a9bb70ff0801d57d5609edb3d742a8
Bhavya Jain <rishikabhavya@gmail.com>
2024-07-27 14:00:26 +0530

    Wide screen issue fix

--- src/components/home/Benefits.tsx
+++ src/components/home/Benefits.tsx
@@ -7,19 +7,21 @@ import BenefitsCard from "./BenefitsCard"
 const LegacyGateway = (): JSX.Element => {
   return (
     <section className="w-full pt-20 p-6 md:pb-20  lg:px-SPACE_16 bg-[#1C1D1F] grid-background">
-      <div>
-        <SectionTitle title="Benefits" />
-        <div className=" h-fit sm:flex-row sm:items-center sm:space-x-SPACE_10 lg:space-x-SPACE_20">
-          <Heading
-            as="h3"
-            className="text-title-large sm:text-display-tiny lg:text-display-small text-white md:w-[65%]"
-          >
-            Tailcall gives you a best-practice GraphQL backend that checks all the boxes.
-          </Heading>
+      <div className="sm:max-w-7xl mx-SPACE_04 sm:mx-SPACE_10 lg:mx-auto  py-SPACE_08 sm:py-SPACE_20">
+        <div>
+          <SectionTitle title="Benefits" />
+          <div className=" h-fit sm:flex-row sm:items-center sm:space-x-SPACE_10 lg:space-x-SPACE_20">
+            <Heading
+              as="h3"
+              className="text-title-large sm:text-display-tiny lg:text-display-small text-white md:w-[65%]"
+            >
+              Tailcall gives you a best-practice GraphQL backend that checks all the boxes.
+            </Heading>
+          </div>
         </div>
+        <BenefitsCard />
+        <Partners />
       </div>
-      <BenefitsCard />
-      <Partners />
     </section>
   )
 }
--- src/components/home/Customers.tsx
+++ src/components/home/Customers.tsx
@@ -6,29 +6,31 @@ import {customerFeedbacks} from "@site/src/constants"
 const Customer = () => {
   return (
     <section className="customer-container !bg-tailCall-dark-600 h-full w-full text-tailCall-light-100 lg:px-SPACE_16 !bg-contain md:!bg-center">
-      <div className="mx-SPACE_04 sm:mx-SPACE_10 lg:mx-auto py-SPACE_20 md:pt-SPACE_40 md:pb-SPACE_20">
-        <div className="flex flex-row items-center justify-center">
-          <Heading
-            as="h5"
-            className="text-title-large sm:text-display-tiny lg:text-display-medium flex flex-col items-center md:flex-row"
-          >
-            <span>Our Customers </span>
-            <span className="bg-tailCall-yellow rounded-lg text-black px-SPACE_01 ml-SPACE_02">love us!</span>
-          </Heading>
-        </div>
-        <div className="flex flex-col space-y-SPACE_10 md:flex-row md:space-x-SPACE_02 md:space-y-0 mt-SPACE_18">
-          {customerFeedbacks.map((feedback) => {
-            return (
-              <CustomerFeedbackCard
-                key={feedback.id}
-                isCenterCard={feedback.id === 2}
-                citation={feedback.citation}
-                designation={feedback.designation}
-                name={feedback?.name}
-                department={feedback?.department}
-              />
-            )
-          })}
+      <div className="sm:max-w-7xl mx-SPACE_04 sm:mx-SPACE_10 lg:mx-auto  py-SPACE_08 sm:py-SPACE_20">
+        <div className="mx-SPACE_04 sm:mx-SPACE_10 lg:mx-auto py-SPACE_20 md:pt-SPACE_40 md:pb-SPACE_20">
+          <div className="flex flex-row items-center justify-center">
+            <Heading
+              as="h5"
+              className="text-title-large sm:text-display-tiny lg:text-display-medium flex flex-col items-center md:flex-row"
+            >
+              <span>Our Customers </span>
+              <span className="bg-tailCall-yellow rounded-lg text-black px-SPACE_01 ml-SPACE_02">love us!</span>
+            </Heading>
+          </div>
+          <div className="flex flex-col space-y-SPACE_10 md:flex-row md:space-x-SPACE_02 md:space-y-0 mt-SPACE_18">
+            {customerFeedbacks.map((feedback) => {
+              return (
+                <CustomerFeedbackCard
+                  key={feedback.id}
+                  isCenterCard={feedback.id === 2}
+                  citation={feedback.citation}
+                  designation={feedback.designation}
+                  name={feedback?.name}
+                  department={feedback?.department}
+                />
+              )
+            })}
+          </div>
         </div>
       </div>
     </section>
