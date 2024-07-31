fda09809f1f886bdc0210e5ec4948b907d077a11
mehulmathur16 <mathur.mehul01@gmail.com>
2024-08-01 01:50:47 +0530

    chore: removed accordion from root & improved styling

--- src/components/playground/templates/ObjectFieldTemplate.tsx
+++ src/components/playground/templates/ObjectFieldTemplate.tsx
@@ -12,33 +12,51 @@ export const ObjectFieldTemplate = (props: ObjectFieldTemplateProps) => {
     }
   }
 
+  const getObjectFieldBody = () => {
+    return (
+      <div>
+        {props.description && <div className="field-description my-SPACE_02">{props.description}</div>}
+        {props.properties.map((element) => (
+          <div className="property-wrapper" key={element.name}>
+            {element.content}
+          </div>
+        ))}
+        {props.schema.additionalProperties && (
+          <IconButton onClick={handleAddClick}>
+            <Add fontSize="small" />
+          </IconButton>
+        )}
+      </div>
+    )
+  }
+
+  const getRootObjectContent = () => {
+    return (
+      <>
+        <div className="mb-SPACE_04 bg-tailCall-yellow rounded-xl sm:rounded-xl px-SPACE_02 w-fit">
+          <Typography variant="h5">{props.title}</Typography>
+        </div>
+        {getObjectFieldBody()}
+      </>
+    )
+  }
+
   return (
     <div>
-      <Accordion>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls={`${props.title}-content`}
-          id={`${props.title}-header`}
-        >
-          <Typography>{props.title}</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <div>
-            {props.description && <Typography>{props.description}</Typography>}
-            {props.properties.map((element) => (
-              <div className="property-wrapper" key={element.name}>
-                {element.content}
-              </div>
-            ))}
-            {props.schema.additionalProperties && (
-              <IconButton onClick={handleAddClick}>
-                <Add fontSize="small" />
-              </IconButton>
-            )}
-          </div>
-        </AccordionDetails>
-      </Accordion>
+      {props?.idSchema["$id"] !== "root" ? (
+        <Accordion>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls={`${props.title}-content`}
+            id={`${props.title}-header`}
+          >
+            <Typography variant={"h5"}>{props.title}</Typography>
+          </AccordionSummary>
+          <AccordionDetails>{getObjectFieldBody()}</AccordionDetails>
+        </Accordion>
+      ) : (
+        getRootObjectContent()
+      )}
     </div>
   )
 }
-
