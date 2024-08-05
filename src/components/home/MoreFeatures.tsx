7cd990eac66a4f4b5cfca336a41abc2a4b41b037
Oms-343 <oms87473@gmail.com>
2024-08-05 22:01:25 +0530

    desktop-styles-completed

--- src/components/home/MoreFeatures.tsx
+++ src/components/home/MoreFeatures.tsx
@@ -18,11 +18,11 @@ type MoreFeaturesProps = {
 const FeatureBox = ({feature}: MoreFeaturesProps) => {
   return (
     <div
-      className=" text-tailCall-light-300 border-2  border-solid  
-     border-tailCall-dark-300 flex items-center gap-x-SPACE_04  py-SPACE_03 px-SPACE_04 rounded-lg shadow-[0px_3px_10px_0px_#000]   lg:rounded-SPACE_4 lg:text-title-tiny  lg:p-[17px]  "
+      className=" text-tailCall-light-300 border-2 border-solid  
+     border-tailCall-dark-300 flex items-center gap-x-SPACE_04 py-SPACE_03 px-SPACE_04 rounded-lg 
+     shadow-[0px_3px_12px_0px_#000] lg:rounded-2xl lg:p-SPACE_06  "
     >
-      <feature.logo className="   w-SPACE_05 h-SPACE_05   lg:w-6 lg:h-6    " />
-
+      <feature.logo className="w-SPACE_05 h-SPACE_05   lg:w-SPACE_06 lg:h-SPACE_06    " />
       <span className="mt-0">{feature.title}</span>
     </div>
   )
@@ -30,16 +30,16 @@ const FeatureBox = ({feature}: MoreFeaturesProps) => {
 
 const MoreFeatures = (): JSX.Element => {
   return (
-    <section className="w-full  mb-SPACE_08 sm:mb-SPACE_20   ">
+    <section className="w-full mb-SPACE_08 sm:mb-SPACE_20">
       <div className="max-w-7xl mx-SPACE_04 sm:mx-SPACE_10 lg:mx-auto   ">
-        <div className="  gap-SPACE_04  mt-SPACE_06 mb-SPACE_12  flex flex-wrap lg:justify-center  lg:gap-x-6 lg:gap-y-6     ">
+        <div className="gap-SPACE_04  mt-SPACE_06 mb-SPACE_12 flex flex-wrap lg:justify-center lg:gap-x-SPACE_06 lg:gap-y-SPACE_08 lg:mt-SPACE_10">
           {moreFeatures.map((feature: MoreFeatures) => (
             <FeatureBox feature={feature} key={feature.id} />
           ))}
         </div>
       </div>
 
-      <div className="hidden lg:flex flex justify-center     ">
+      <div className="hidden lg:flex justify-center">
         <LinkButton
           title="View More"
           onClick={() => analyticsHandler("Home Page", "Click", "View More")}
@@ -48,7 +48,7 @@ const MoreFeatures = (): JSX.Element => {
           width="large"
         />
       </div>
-      <div className=" lg:hidden  flex justify-center ">
+      <div className="lg:hidden flex justify-center">
         <LinkButton
           title="View More"
           onClick={() => analyticsHandler("Home Page", "Click", "View More")}
