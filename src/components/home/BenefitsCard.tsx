7138c2d5bbefe7f522fb214049b191dab1373cb4
Jay Kumar <jaykumar20march@gmail.com>
2024-08-11 11:07:14 +0000

    fix: replace div with anchor

--- src/components/home/BenefitsCard.tsx
+++ src/components/home/BenefitsCard.tsx
@@ -8,26 +8,28 @@ const BenefitsCard = (): JSX.Element => {
       <div className="md:flex md:flex-wrap justify-center gap-10 w-[100%]">
         {benefits.map((item) => (
           <a
-            className="group border border-solid border-tailCall-border-dark-300 rounded-3xl md:w-[40%] lg:w-[45%] md:px-10 px-4 py-2 pt-4 lg:flex benefits-drop-shadow hover:border-[#FDEA2E] md:mb-0 mb-6 cursor-pointer hover:no-underline"
+            className="md:w-[40%] lg:w-[45%] lg:flex !no-underline"
             key={item.id}
             href={item.redirection_url}
             target="_blank"
           >
-            <div className="mr-4 md:mt-4">
-              <img
-                src={item.image}
-                alt="Image Describing Why Tailcall"
-                className="max-w-[72px] lg:w-[64px] sm:max-w-[110px]"
-              />
-            </div>
-            <div className="mt-4 flex flex-col">
-              <p className="text-title-small text-white sm:text-title-large mb-SPACE_02 sm:mb-0 flex flex-start">
-                {item.title}{" "}
-                <span className="ml-4 md:text-gray-400 group-hover:text-white">
-                  <ArrowRight />
-                </span>
-              </p>
-              <p className="text-content-tiny sm:text-content-small text-tailCall-light-600">{item.description}</p>
+            <div className="group border border-solid border-tailCall-border-dark-300 rounded-3xl  md:px-10 px-4 py-2 pt-4  benefits-drop-shadow hover:border-[#FDEA2E] md:mb-0 mb-6 cursor-pointer">
+              <div className="mr-4 md:mt-4">
+                <img
+                  src={item.image}
+                  alt="Image Describing Why Tailcall"
+                  className="max-w-[72px] lg:w-[64px] sm:max-w-[110px]"
+                />
+              </div>
+              <div className="mt-4 flex flex-col">
+                <p className="text-title-small text-white sm:text-title-large mb-SPACE_02 sm:mb-0 flex flex-start">
+                  {item.title}{" "}
+                  <span className="ml-4 md:text-gray-400 group-hover:text-white">
+                    <ArrowRight />
+                  </span>
+                </p>
+                <p className="text-content-tiny sm:text-content-small text-tailCall-light-600">{item.description}</p>
+              </div>
             </div>
           </a>
         ))}
