d3c8387e9ee86e2b406c9b4b672f0346d4058445
Dhairya Majmudar <2022kuec2045@iiitkota.ac.in>
2024-08-15 17:58:15 +0530

    align

--- src/components/home/Benefits.tsx
+++ src/components/home/Benefits.tsx
@@ -8,9 +8,9 @@ const Benefits = (): JSX.Element => {
   return (
     <section className="w-full pt-20 p-6 md:pb-20  lg:px-SPACE_16 bg-[#1C1D1F] grid-background">
       <div className="sm:max-w-7xl mx-SPACE_04 sm:mx-SPACE_10 lg:mx-auto  py-SPACE_08 sm:py-SPACE_20">
-        <div>
+        <div className="ml-10">
           <SectionTitle title="Benefits" />
-          <div className=" h-fit sm:flex-row sm:items-center sm:space-x-SPACE_10 lg:space-x-SPACE_20">
+          <div className=" h-fit sm:flex-row sm:items-center sm:space-x-SPACE_10 lg:space-x-SPACE_20 ">
             <Heading
               as="h3"
               className="text-title-large sm:text-display-tiny lg:text-display-small text-white md:w-[65%]"
