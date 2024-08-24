200a6fa26cf94d5fc5a8de8b3b161b9e00a0ecf6
amit <amitksingh1490@gmail.com>
2024-08-24 12:54:49 +0530

    chore: banner

---
+++ src/components/shared/Announcement.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+interface AnnouncementProps {
+  text: string;
+  refLink?: string;
+  refText?: string;
+}
+
+const Announcement: React.FC<AnnouncementProps> = ({ text, refLink, refText }) => {
+  return (
+    <div className="w-full h-[39px] bg-black text-white flex items-center justify-center">
+      <div className="text-sm sm:text-base md:text-lg font-bold">
+        {text}
+        {refLink && refText && (
+          <a className="text-tailCall-yellow font-bold" href={refLink}> {refText} </a>
+        )
+        }
+      </div>
+    </div>
+  );
+};
+
+export default Announcement;
