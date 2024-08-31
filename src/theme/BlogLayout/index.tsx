c2905659d2d52cd8614e35fa12bfccc4125363a8
neo773 <62795688+neo773@users.noreply.github.com>
2024-08-31 14:58:59 +0530

    rm used dependency

--- src/theme/BlogLayout/index.tsx
+++ src/theme/BlogLayout/index.tsx
@@ -3,14 +3,10 @@ import clsx from "clsx"
 import Layout from "@theme/Layout"
 import type {Props} from "@theme/BlogLayout"
 import BlogRecentPosts from "../BlogRecentPosts"
-import {useLocation} from "@docusaurus/router"
 
 export default function BlogLayout(props: Props): JSX.Element {
   const {sidebar, toc, children, ...layoutProps} = props
   const hasSidebar = sidebar && sidebar.items.length > 0
-  const location = useLocation()
-  console.log(location, "location")
-  const isBlogRoot = location.pathname === "/blog" || location.pathname === "/blog/"
 
   return (
     <Layout {...layoutProps}>
