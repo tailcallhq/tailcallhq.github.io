dd09562614ce188c60485a6ce519d9d4ef38b570
neo773 <62795688+neo773@users.noreply.github.com>
2024-09-07 09:29:14 +0530

    fix(479): Searchbar (#480)

--- src/theme/DocRoot/Layout/Sidebar/index.tsx
+++ src/theme/DocRoot/Layout/Sidebar/index.tsx
@@ -1,65 +1,148 @@
-import React, {type ReactNode, useState, useCallback} from "react"
-import clsx from "clsx"
-import {prefersReducedMotion, ThemeClassNames} from "@docusaurus/theme-common"
-import {useDocsSidebar} from "@docusaurus/theme-common/internal"
-import {useLocation} from "@docusaurus/router"
-import DocSidebar from "@theme/DocSidebar"
-import ExpandButton from "@theme/DocRoot/Layout/Sidebar/ExpandButton"
-import type {Props} from "@theme/DocRoot/Layout/Sidebar"
+import React, {useEffect, useState, useRef} from "react"
+import {useHistory} from "react-router-dom"
+import Sidebar from "@theme-original/DocRoot/Layout/Sidebar"
+import Search from "docusaurus-lunr-search/src/theme/SearchBar"
+import useIsBrowser from "@docusaurus/useIsBrowser"
+import Platform from "react-platform-js"
 
+import PageSearchIcon from "@site/static/icons/basic/page-search.svg"
+import EnterKeyIcon from "@site/static/icons/basic/enter-key.svg"
+import UpDownKeyIcon from "@site/static/icons/basic/up-down-key.svg"
+import EscapeKeyIcon from "@site/static/icons/basic/escape-key.svg"
 import styles from "./styles.module.css"
+import {getSearchInputRef, setBodyOverflow} from "@site/src/utils"
 
-// Reset sidebar state when sidebar changes
-// Use React key to unmount/remount the children
-// See https://github.com/facebook/docusaurus/issues/3414
-function ResetOnSidebarChange({children}: {children: ReactNode}) {
-  const sidebar = useDocsSidebar()
-  return <React.Fragment key={sidebar?.name ?? "noSidebar"}>{children}</React.Fragment>
-}
+const CustomSearch = () => {
+  const [isSearchModalVisible, setIsSearchModalVisible] = useState<boolean>(false)
+  const focusRef = useRef()
+  const history = useHistory()
+  const isBrowser = useIsBrowser()
+  const placeholder = isBrowser ? (Platform.OS.startsWith("Mac") ? "Search ⌘+K" : "Search Ctrl+K") : "Search"
 
-export default function DocRootLayoutSidebar({
-  sidebar,
-  hiddenSidebarContainer,
-  setHiddenSidebarContainer,
-}: Props): JSX.Element {
-  const {pathname} = useLocation()
+  // Function to handle opening the search modal
+  const handleSearchClick = () => {
+    setIsSearchModalVisible(true)
+  }
 
-  const [hiddenSidebar, setHiddenSidebar] = useState(false)
-  const toggleSidebar = useCallback(() => {
-    if (hiddenSidebar) {
-      setHiddenSidebar(false)
+  // Function to handle closing the search modal
+  const handleSearchModalClose = () => {
+    setIsSearchModalVisible(false)
+  }
+
+  // Function to control body scroll based on modal visibility
+  const setBodyScroll = () => {
+    if (isSearchModalVisible) {
+      setBodyOverflow("hidden")
+    } else {
+      setBodyOverflow("initial")
     }
-    // onTransitionEnd won't fire when sidebar animation is disabled
-    // fixes https://github.com/facebook/docusaurus/issues/8918
-    if (!hiddenSidebar && prefersReducedMotion()) {
-      setHiddenSidebar(true)
+  }
+
+  // Function to handle key press events
+  const handleKeyPress = (event: KeyboardEvent) => {
+    if (event.key === "Escape") {
+      handleSearchModalClose()
     }
-    setHiddenSidebarContainer((value) => !value)
-  }, [setHiddenSidebarContainer, hiddenSidebar])
+    if (
+      (event.metaKey && event.key === "k" && Platform.UA.includes("Mac")) ||
+      (event.ctrlKey && event.key === "k" && Platform.UA.includes("Win"))
+    ) {
+      handleSearchClick()
+    }
+  }
+
+  useEffect(() => {
+    // handle body scroll based on modal visibility changes
+    setBodyScroll()
+
+    // handle keydown events for search functionality
+    document.addEventListener("keydown", handleKeyPress)
+
+    // close the search modal when route changes
+    const unlisten = history.listen((location, action) => {
+      if (action === "PUSH" || action === "POP") {
+        setIsSearchModalVisible(false)
+      }
+    })
+
+    const focusSearchBar = () => {
+      const searchInput = getSearchInputRef()
+      if (searchInput && focusRef.current !== "Loading...") {
+        setTimeout(() => {
+          searchInput.focus()
+        }, 20)
+      }
+    }
+
+    const searchContainer = document.getElementById("search-container")
+    if (searchContainer) {
+      searchContainer.addEventListener("DOMSubtreeModified", focusSearchBar)
+    }
+
+    // Cleanup (when component unmounts or dependencies change)
+    return () => {
+      setBodyOverflow("initial")
+      document.removeEventListener("keydown", handleKeyPress)
+      unlisten()
+
+      if (searchContainer) {
+        searchContainer.removeEventListener("DOMSubtreeModified", focusSearchBar)
+      }
+    }
+  }, [history, isSearchModalVisible])
 
   return (
-    <aside
-      className={clsx(
-        ThemeClassNames.docs.docSidebarContainer,
-        styles.docSidebarContainer,
-        hiddenSidebarContainer && styles.docSidebarContainerHidden,
-      )}
-      onTransitionEnd={(e) => {
-        if (!e.currentTarget.classList.contains(styles.docSidebarContainer!)) {
-          return
-        }
+    <>
+      {/* Search input container */}
+      <div className={styles.inputContainer} onClick={handleSearchClick}>
+        <span aria-label="expand searchbar" role="button" className="search-icon" tabIndex={0}></span>
+        <input readOnly placeholder={placeholder} className={styles.input} />
+      </div>
 
-        if (hiddenSidebarContainer) {
-          setHiddenSidebar(true)
-        }
-      }}
-    >
-      <ResetOnSidebarChange>
-        <div className={clsx(styles.sidebarViewport, hiddenSidebar && styles.sidebarViewportHidden)}>
-          <DocSidebar sidebar={sidebar} path={pathname} onCollapse={toggleSidebar} isHidden={hiddenSidebar} />
-          {hiddenSidebar && <ExpandButton toggleSidebar={toggleSidebar} />}
-        </div>
-      </ResetOnSidebarChange>
-    </aside>
+      {/* Search modal */}
+      <div id="search-container">
+        {isSearchModalVisible ? (
+          <>
+            <div onClick={handleSearchModalClose} className={styles.overlay}></div>
+            <div className={styles.modal}>
+              <div className={styles.modalContent}>
+                <Search />
+                <div className={styles.initialCase}>
+                  <PageSearchIcon />
+                  <div className={styles.searchDocsTitle}>Search Docs</div>
+                  <div className={styles.searchDocsDesc}>Search anything within the docs</div>
+                </div>
+              </div>
+              <div className={styles.footer}>
+                <div className={styles.navigationInfoItem}>
+                  <EnterKeyIcon />
+                  <span>to select</span>
+                </div>
+                <div className={styles.navigationInfoItem}>
+                  <UpDownKeyIcon />
+                  <span>to navigate</span>
+                </div>
+                <div className={styles.navigationInfoItem}>
+                  <EscapeKeyIcon />
+                  <span>to close</span>
+                </div>
+              </div>
+            </div>
+          </>
+        ) : null}
+      </div>
+    </>
   )
 }
+
+// Wrapper component combining Sidebar with CustomSearch
+const SidebarWrapper = (props: SidebarConfig) => {
+  return (
+    <div className="sidebar-search-container flex flex-col lg:mb-[100px]">
+      <CustomSearch />
+      <Sidebar {...props} />
+    </div>
+  )
+}
+
+export default SidebarWrapper
