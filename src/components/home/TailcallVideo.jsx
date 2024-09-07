80c80f609f141e2964799502beefae44037a465c
Bhavya Jain <rishikabhavya@gmail.com>
2024-09-07 10:24:26 +0530

    wide sreen fix

--- src/components/home/TailcallVideo.jsx
+++ src/components/home/TailcallVideo.jsx
@@ -2,19 +2,21 @@ import React from "react"
 
 const TailcallVideo = () => {
   return (
-    <div>
-      <div className="w-[100%] mb-[-10px]">
-        <iframe
-          width="100%"
-          height="600vh"
-          src="https://www.youtube.com/embed/-hgzB5JhWJQ?rel=0&controls=0&modestbranding=1&iv_load_policy=3"
-          title="YouTube video player"
-          frameBorder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          allowFullScreen
-        ></iframe>
+    <section className="w-full px-8 py-6 md:px-24 lg:px-36 lg:py-20 bg-tailCall-dark-600 h-full  text-tailCall-light-100 ">
+      <div className="w-full max-w-7xl mx-auto">
+        <div className="w-[100%] mb-[-10px]">
+          <iframe
+            width="100%"
+            height="600vh"
+            src="https://www.youtube.com/embed/-hgzB5JhWJQ?rel=0&controls=0&modestbranding=1&iv_load_policy=3"
+            title="YouTube video player"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowFullScreen
+          ></iframe>
+        </div>
       </div>
-    </div>
+    </section>
   )
 }
 
