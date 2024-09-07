77369c86533e112f26bc9775a7dede08b5b2dbb9
Akhilender <akhilenderb9@gmail.com>
2024-09-07 10:44:28 +0530

    fix: Removed YouTube controls to improve alignment with website branding
    Signed-off-by: Akhilender <akhilenderb9@gmail.com>

--- src/components/home/TailcallIntroVideo.tsx
+++ src/components/home/TailcallIntroVideo.tsx
@@ -5,7 +5,7 @@ const TailcallIntroVideo = () => {
     <div className="relative pb-[56.25%] h-0 overflow-hidden max-w-full bg-black">
       <iframe
         className="absolute top-0 left-0 w-full h-full border-0"
-        src="https://www.youtube.com/embed/-hgzB5JhWJQ"
+        src="https://www.youtube.com/embed/-hgzB5JhWJQ?controls=0"
         title="Tailcall Introduction Video"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
         allowFullScreen
