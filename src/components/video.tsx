7dba5ecbd9014f12e63a7336e5e1df8f66d6123e
Tushar Mathur <tusharmath@gmail.com>
2024-09-09 10:32:40 -0700

    add getting started video

---
+++ src/components/video.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function Video() {
+  return (
+    <div>
+      <iframe
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/-hgzB5JhWJQ?si=xhDxmw5--9Qu11IL&rel=0"
+        title="YouTube video player"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerPolicy="strict-origin-when-cross-origin"
+        allowFullScreen
+      ></iframe>
+    </div>
+  )
+}
