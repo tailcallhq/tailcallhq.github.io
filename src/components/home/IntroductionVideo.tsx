1fb98696cea34ceb7950e3a634b87b5d77d1f480
neo773 <62795688+neo773@users.noreply.github.com>
2024-09-09 21:26:02 +0530

    changes

--- src/components/home/IntroductionVideo.tsx
+++ src/components/home/IntroductionVideo.tsx
@@ -1,47 +1,20 @@
-import React, {useEffect, useState} from "react"
-
-interface NoEmbedResponse {
-  html: string
-  provider_url: string
-  type: string
-  height: number
-  title: string
-  thumbnail_url: string
-  author_name: string
-  author_url: string
-  provider_name: string
-  thumbnail_width: number
-  width: number
-  version: string
-  thumbnail_height: number
-  url: string
-}
+import React, {useState} from "react"
 
 const VIDEO_ID = "-hgzB5JhWJQ"
 
 const Youtube: React.FC = () => {
-  const [videoData, setVideoData] = useState<NoEmbedResponse | null>(null)
   const [isPlaying, setIsPlaying] = useState(false)
-
-  const fetchNoEmbed = async (videoID: string): Promise<NoEmbedResponse> => {
-    const response = await fetch(`https://noembed.com/embed?url=https://www.youtube.com/watch?v=${videoID}`)
-    return (await response.json()) as NoEmbedResponse
-  }
 
   const handlePlay = () => {
     setIsPlaying(true)
   }
-
-  useEffect(() => {
-    fetchNoEmbed(VIDEO_ID).then((data) => setVideoData(data))
-  }, [])
 
   return (
     <div className="video-container">
       <div
         className="absolute top-0 left-0 w-full h-full bg-cover bg-center 3xl:rounded-3xl"
         style={{
-          backgroundImage: `url(${videoData?.thumbnail_url})`,
+          backgroundImage: `url(/images/home/introduction-video-thumbnail.png)`,
         }}
       >
         {!isPlaying ? (
@@ -49,9 +22,12 @@ const Youtube: React.FC = () => {
             <div className="absolute top-0 left-0 bottom-0 right-0 flex items-center justify-center">
               <div
                 onClick={handlePlay}
-                className="w-14 h-14 md:w-[120px] md:h-[120px] bg-gradient-to-t from-yellow-200/20 to-transparent border md:border-[3px] border-solid border-yellow-500 shadow-md backdrop-blur-xl rounded-[20px] md:rounded-[42px] flex items-center justify-center cursor-pointer"
+                className="w-14 h-14 md:w-[120px] md:h-[120px] bg-white border md:border-[3px] border-solid border-tailCall-yellow shadow-md backdrop-blur-xl rounded-[20px] md:rounded-[42px] flex items-center justify-center cursor-pointer"
+                style={{
+                  boxShadow: "0px 22.22px 111.11px 0px #1B19444D",
+                }}
               >
-                <img src="/icons/basic/play.svg" alt="Play Button Icon" className="w-6 h-6 md:w-12 md:h-12" />
+                <img src="/icons/basic/play.svg" alt="Play Button Icon" className="size-6 md:size-10" />
               </div>
             </div>
             <div className="absolute bottom-0 left-0 w-full h-20 md:h-64 bg-gradient-to-t from-tailCall-yellow/15 to-transparent"></div>
@@ -62,7 +38,7 @@ const Youtube: React.FC = () => {
             src={`https://www.youtube.com/embed/${VIDEO_ID}?feature=oembed&autoplay=1`}
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             allowFullScreen
-            title={videoData?.title}
+            title="Tailcall Introduction Video"
           />
         )}
       </div>
