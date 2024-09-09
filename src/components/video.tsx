905d7f080446a14e1256c36693ce579e36978b63
Tushar Mathur <tusharmath@gmail.com>
2024-09-09 13:23:54 -0700

    make video responsive

--- src/components/video.tsx
+++ src/components/video.tsx
@@ -2,10 +2,8 @@ import React from "react"
 
 export default function Video() {
   return (
-    <div>
+    <div className="responsive-iframe-container">
       <iframe
-        width="560"
-        height="315"
         src="https://www.youtube.com/embed/-hgzB5JhWJQ?si=xhDxmw5--9Qu11IL&rel=0"
         title="YouTube video player"
         frameBorder="0"
