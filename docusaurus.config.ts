e0f6ba62c3e69e328cfa7ccbd10bb3b8d8d5398b
Sahil Yeole <sahilyeole93@gmail.com>
2024-09-25 10:07:44 +0530

    fix: css
    Signed-off-by: Sahil Yeole <sahilyeole93@gmail.com>

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -257,12 +257,18 @@ export default {
         sidebarPath: require.resolve("./graphql/sidebar.ts"),
       },
     ],
-    [
-      "@docusaurus/plugin-ideal-image",
-      {
-        highlightResult: true,
-      },
-    ],
+    // [
+    // "@docusaurus/plugin-ideal-image",
+    // {
+    //   {
+    //     // quality: 70,
+    //     // max: 1030, // max resized image's size.
+    //     // min: 640, // min resized image's size. if original is lower, use that size.
+    //     // steps: 2, // the max number of images generated between min and max (inclusive)
+    //     disableInDev: false,
+    //   },
+    // },
+    // ],
     // causing problem in layouts and image loads
     async function myPlugin() {
       return {
