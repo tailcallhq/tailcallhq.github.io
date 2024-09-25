b8c05d3f01b6eacf9eab63e57497056a7b32d50e
neo773 <neo773@protonmail.com>
2024-09-25 21:54:50 +0530

    use a new approach

--- src/components/home/IntroductionVideo.tsx
+++ src/components/home/IntroductionVideo.tsx
@@ -1,15 +1,46 @@
-import React, {useState} from "react"
+import React, {useState, useEffect, useRef} from "react"
 
 const IntroductionVideo: React.FC = () => {
   const [isPlaying, setIsPlaying] = useState(false)
+  const [thumbnailUrl, setThumbnailUrl] = useState("")
+  const videoId = "1011521201"
+  const videoRef = useRef<HTMLDivElement>(null)
+
+  const fetchThumbnail = async () => {
+    const response = await fetch(`https://vimeo.com/api/oembed.json?url=https://vimeo.com/${videoId}`)
+    const data = await response.json()
+    const fullResUrl = data.thumbnail_url.split("-d")[0] + "-d"
+    setThumbnailUrl(fullResUrl)
+  }
+
+  useEffect(() => {
+    fetchThumbnail()
+
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          setIsPlaying(true)
+        }
+      },
+      {rootMargin: "200px"},
+    )
+
+    if (videoRef.current) {
+      observer.observe(videoRef.current)
+    }
+
+    return () => {
+      observer.disconnect()
+    }
+  }, [])
 
   return (
-    <div className="video-wrapper">
+    <div className="video-wrapper" ref={videoRef}>
       <div className="video-container">
         <div
           className="video-background"
           style={{
-            backgroundImage: `url(/images/home/introduction-video-thumbnail.png)`,
+            backgroundImage: `url(${thumbnailUrl})`,
           }}
         >
           {!isPlaying ? (
@@ -18,7 +49,7 @@ const IntroductionVideo: React.FC = () => {
             </div>
           ) : (
             <iframe
-              src="https://player.vimeo.com/video/1011521201?autoplay=1&badge=0&autopause=0&player_id=0&app_id=58479"
+              src={`https://player.vimeo.com/video/${videoId}?autoplay=0&badge=0&autopause=0&player_id=0&app_id=58479`}
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
               allowFullScreen
               className="absolute top-0 left-0 w-full h-full"
