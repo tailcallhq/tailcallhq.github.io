0c24383aa5719862219d65abb07f1420e55cfee3
Sahil Yeole <sahilyeole93@gmail.com>
2024-09-25 10:28:52 +0530

    delete commented code
    Signed-off-by: Sahil Yeole <sahilyeole93@gmail.com>

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -257,19 +257,6 @@ export default {
         sidebarPath: require.resolve("./graphql/sidebar.ts"),
       },
     ],
-    // [
-    // "@docusaurus/plugin-ideal-image",
-    // {
-    //   {
-    //     // quality: 70,
-    //     // max: 1030, // max resized image's size.
-    //     // min: 640, // min resized image's size. if original is lower, use that size.
-    //     // steps: 2, // the max number of images generated between min and max (inclusive)
-    //     disableInDev: false,
-    //   },
-    // },
-    // ],
-    // causing problem in layouts and image loads
     async function myPlugin() {
       return {
         name: "docusaurus-tailwindcss",
