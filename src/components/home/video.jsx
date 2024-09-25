ca657bef38c3c7becefeee8a754bfeb3a925d9ae
= <=>
2024-09-25 18:08:14 +0500

    add shadow and classes

--- src/components/home/video.jsx
+++ src/components/home/video.jsx
@@ -1,12 +1,15 @@
 export default function Video() {
   return (
-    <div className=" grid grid-light items-center p-12 justify-items-center">
-      <div style={{padding: "56.25% 0 0 0", borderRadius: 24}} className="relative overflow-clip w-[100%] h-[100%]">
+    <div className="grid grid-light items-center p-12 justify-items-center">
+      <div
+        style={{padding: "56.25% 0 0 0", borderRadius: 24, boxShadow: "rgba(0, 0, 0) 0px 0px 20px 0px"}}
+        className="  relative overflow-clip w-[100%] h-[100%]"
+      >
         <iframe
           src="https://player.vimeo.com/video/1011521201?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479"
           frameborder="0"
           allow="autoplay; fullscreen; picture-in-picture; clipboard-write"
-          style={{position: "absolute", top: 0, left: 0, width: "100%", height: "100%", zIndex: 100}}
+          className="absolute top-0 left-0 w-full h-full z-50"
           title="Tailcall Introduction"
         ></iframe>
       </div>
