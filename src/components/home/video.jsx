36330c3d6fce1036f37ab39796535548a7280c4d
= <=>
2024-09-25 18:34:17 +0500

    apply requested changes

--- src/components/home/video.jsx
+++ src/components/home/video.jsx
@@ -1,9 +1,9 @@
 export default function Video() {
   return (
-    <div className="grid grid-light items-center p-12 justify-items-center">
+    <div className="grid grid-light items-center sm:p-12 p-0 justify-items-center">
       <div
-        style={{padding: "56.25% 0 0 0", borderRadius: 24, boxShadow: "rgb(0 0 0 / 62%) 0px 0px 10px 0px"}}
-        className="  relative overflow-clip w-[100%] h-[100%]"
+        style={{padding: "56.25% 0 0 0", boxShadow: "rgb(0 0 0 / 62%) 0px 0px 10px 0px"}}
+        className="  sm:rounded-[24px] rounded-none  relative overflow-clip w-[100%] h-[100%]"
       >
         <iframe
           src="https://player.vimeo.com/video/1011521201?badge=0&amp;autopause=0&amp;autoplay=1&amp;player_id=0&amp;app_id=58479"
