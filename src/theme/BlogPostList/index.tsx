f4d6c300ab55938ee5e4f207a29dd026477b3fc5
neo773 <neo773@protonmail.com>
2024-09-28 21:55:50 +0530

    changes

--- src/theme/BlogPostList/index.tsx
+++ src/theme/BlogPostList/index.tsx
@@ -12,7 +12,7 @@ const FeaturedPost = ({item}: {item: Props["items"][0]}) => (
     <img
       src={item.content.metadata.frontMatter.image}
       alt={item.content.metadata.title}
-      className="w-full object-cover h-96"
+      className="w-full object-cover aspect-video"
     />
     <PostContent item={item} isFeatured />
   </Link>
