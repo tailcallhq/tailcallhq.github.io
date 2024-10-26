f389e3ba52077c212f34c962b8830ebcb77ff73b
Mehul Mathur <mathur.mehul01@gmail.com>
2024-10-26 07:35:51 +0000

    styles revert

--- src/theme/BlogCategories/index.tsx
+++ src/theme/BlogCategories/index.tsx
@@ -31,7 +31,7 @@ export function BlogCategories({items, onCategoryClick, activeCategory}: BlogCat
           className={clsx(
             "text-content-small md:text-title-tiny cursor-pointer appearance-none border-none bg-transparent px-1",
             activeCategory === name
-              ? "!font-medium text-black blog-category-active-tab"
+              ? "!font-medium text-black border-b-solid border-b-2 border-black"
               : "!font-normal text-tailCall-dark-200 hover:text-gray-700",
           )}
         >
