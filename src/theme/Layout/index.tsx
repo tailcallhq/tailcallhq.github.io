7249abbc69371eddf799ad101cfa39e05afd7005
Mehul Mathur <mathur.mehul01@gmail.com>
2024-11-22 20:44:41 +0000

    chore: swizzled layout/index

--- src/theme/Layout/index.tsx
+++ src/theme/Layout/index.tsx
@@ -1,44 +1,56 @@
-import React, {useEffect} from "react"
-import Layout from "@theme-original/Layout"
-import type LayoutType from "@theme/Layout"
-import type {WrapperProps} from "@docusaurus/types"
-import GlobalHead from "@site/src/components/shared/GlobalHead"
-import CookieConsentModal from "@site/src/components/shared/CookieConsentModal/CookieConsentModal"
-import {useCookieConsentManager} from "@site/src/utils/hooks/useCookieConsentManager"
-import {pageLinks} from "@site/src/constants/routes"
+import React from 'react';
+import clsx from 'clsx';
+import ErrorBoundary from '@docusaurus/ErrorBoundary';
+import {
+  PageMetadata,
+  SkipToContentFallbackId,
+  ThemeClassNames,
+} from '@docusaurus/theme-common';
+import {useKeyboardNavigation} from '@docusaurus/theme-common/internal';
+import SkipToContent from '@theme/SkipToContent';
+import AnnouncementBar from '@theme/AnnouncementBar';
+import Navbar from '@theme/Navbar';
+import Footer from '@theme/Footer';
+import LayoutProvider from '@theme/Layout/Provider';
+import ErrorPageContent from '@theme/ErrorPageContent';
+import type {Props} from '@theme/Layout';
+import styles from './styles.module.css';
 
-type Props = WrapperProps<typeof LayoutType>
-
-export default function LayoutWrapper(props: Props): JSX.Element {
+export default function Layout(props: Props): JSX.Element {
   const {
-    isCookieConsentModalVisible,
-    openCookieConsentModal,
-    closeCookieConsentModal,
-    onAccept,
-    onDeny,
-    onPartialAccept,
-    cookieConsent,
-  } = useCookieConsentManager()
+    children,
+    noFooter,
+    wrapperClassName,
+    // Not really layout-related, but kept for convenience/retro-compatibility
+    title,
+    description,
+  } = props;
 
-  useEffect(() => {
-    if (typeof window !== "undefined" && window.location.pathname.includes(pageLinks.privacyPolicy)) return
-
-    if (!cookieConsent) {
-      openCookieConsentModal()
-    }
-  }, [cookieConsent])
+  useKeyboardNavigation();
 
   return (
-    <>
-      <CookieConsentModal
-        open={isCookieConsentModalVisible}
-        onClose={closeCookieConsentModal}
-        onAccept={onAccept}
-        onDeny={onDeny}
-        onPartialAccept={onPartialAccept}
-      />
-      <GlobalHead isCookieConsentAccepted={Boolean(cookieConsent?.accepted)} preferences={cookieConsent?.preferences} />
-      <Layout {...props} />
-    </>
-  )
+    <LayoutProvider>
+      <PageMetadata title={title} description={description} />
+
+      <SkipToContent />
+
+      <AnnouncementBar />
+
+      <Navbar />
+
+      <div
+        id={SkipToContentFallbackId}
+        className={clsx(
+          ThemeClassNames.wrapper.main,
+          styles.mainWrapper,
+          wrapperClassName,
+        )}>
+        <ErrorBoundary fallback={(params) => <ErrorPageContent {...params} />}>
+          {children}
+        </ErrorBoundary>
+      </div>
+
+      {!noFooter && <Footer />}
+    </LayoutProvider>
+  );
 }
